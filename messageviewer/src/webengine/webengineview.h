1040e170b3c0200c37dee991c7af0af194736fc9
Montel Laurent <montel@kde.org>
2016-03-16 21:34:45 +0100

    Add webengineview class

---
+++ messageviewer/src/webengine/webengineview.h
@@ -0,0 +1,37 @@
+/*
+  Copyright (c) 2016 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#ifndef WEBENGINEVIEW_H
+#define WEBENGINEVIEW_H
+
+#include <QWebEngineView>
+#include "messageviewer_export.h"
+
+namespace MessageViewer
+{
+class WebEngineViewPrivate;
+class MESSAGEVIEWER_EXPORT WebEngineView : public QWebEngineView
+{
+    Q_OBJECT
+public:
+    explicit WebEngineView(QWidget *parent = Q_NULLPTR);
+    ~WebEngineView();
+private:
+    WebEngineViewPrivate *const d;
+};
+}
+#endif // WEBENGINEVIEW_H
