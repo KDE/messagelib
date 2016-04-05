e84e72ca36492cdc61c0084e050c22490344b4d9
Montel Laurent <montel@kde.org>
2016-04-05 13:14:16 +0200

    Add autotests

---
+++ messageviewer/src/webengine/autotests/webhittestresulttest.h
@@ -0,0 +1,38 @@
+/*
+  Copyright (c) 2016 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+
+#ifndef WEBHITTESTRESULTTEST_H
+#define WEBHITTESTRESULTTEST_H
+
+#include <QObject>
+
+class WebHitTestResultTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit WebHitTestResultTest(QObject *parent = Q_NULLPTR);
+    ~WebHitTestResultTest();
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+    void shouldAssignPosAndUrl();
+    void shouldAssignFromQVariant();
+
+
+};
+
+#endif // WEBHITTESTRESULTTEST_H
