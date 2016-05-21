d32aeb970ad823a4027c80c93fe2eb4bb11f6608
Montel Laurent <montel@kde.org>
2016-05-21 15:29:03 +0200

    Add more autotests

--- webengineviewer/src/print/autotests/printwebengineviewjobtest.h
+++ webengineviewer/src/print/autotests/printwebengineviewjobtest.h
@@ -29,6 +29,7 @@ public:
 private Q_SLOTS:
     void shouldHaveDefaultValue();
     void shouldFailedWhenWebEngineIsNotDefined();
+    void shouldFailedWhenPageLayoutIsNotDefined();
 };
 
 #endif // PRINTWEBENGINEVIEWJOBTEST_H
