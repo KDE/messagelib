f93c9316cce7c7159ab5760136a1d4964d6658b0
Montel Laurent <montel@kde.org>
2016-05-24 08:13:12 +0200

    fix test

--- messageviewer/src/viewer/webengine/tests/testmaildndattachment.cpp
+++ messageviewer/src/viewer/webengine/tests/testmaildndattachment.cpp
@@ -34,10 +34,68 @@ TestMailDndAttachment::TestMailDndAttachment(QWidget *parent)
     QVBoxLayout *vbox = new QVBoxLayout;
     setLayout(vbox);
 
+    KMime::Message *msg = new KMime::Message;
+
+    QByteArray mail = "From: Thomas McGuire <dontspamme@gmx.net>"
+                      "Subject: Fwd: Test with attachment"
+                      "Date: Wed, 5 Aug 2009 10:58:27 +0200"
+                      "MIME-Version: 1.0"
+                      "Content-Type: Multipart/Mixed;"
+                      "  boundary=\"Boundary-00=_zmUeKB+A8hGfCVZ\""
+                      ""
+                      ""
+                      "--Boundary-00=_zmUeKB+A8hGfCVZ"
+                      "Content-Type: text/plain;"
+                      "  charset=\"iso-8859-15\""
+                      "Content-Transfer-Encoding: 7bit"
+                      "Content-Disposition: inline"
+                      ""
+                      "This is the first encapsulating message."
+                      ""
+                      "--Boundary-00=_zmUeKB+A8hGfCVZ"
+                      "Content-Type: message/rfc822;"
+                      "  name=\"forwarded message\""
+                      "Content-Transfer-Encoding: 7bit"
+                      "Content-Description: Thomas McGuire <dontspamme@gmx.net>: Test with attachment"
+                      "Content-Disposition: inline"
+                      ""
+                      "From: Thomas McGuire <dontspamme@gmx.net>"
+                      "Subject: Test with attachment"
+                      "Date: Wed, 5 Aug 2009 10:57:58 +0200"
+                      "MIME-Version: 1.0"
+                      "Content-Type: Multipart/Mixed;"
+                      "  boundary=\"Boundary-00=_WmUeKQpGb0DHyx1\""
+                      ""
+                      "--Boundary-00=_WmUeKQpGb0DHyx1"
+                      "Content-Type: text/plain;"
+                      "  charset=\"us-ascii\""
+                      "Content-Transfer-Encoding: 7bit"
+                      "Content-Disposition: inline"
+                      ""
+                      ""
+                      ""
+                      ""
+                      "This is the second encapsulated message."
+                      ""
+                      "--Boundary-00=_WmUeKQpGb0DHyx1"
+                      "Content-Type: text/plain;"
+                      "  name=\"attachment.txt\""
+                      "Content-Transfer-Encoding: 7bit"
+                      "Content-Disposition: attachment;"
+                      "  filename=\"attachment.txt\""
+                      ""
+                      "This is an attachment."
+                      ""
+                      "--Boundary-00=_WmUeKQpGb0DHyx1--"
+                      ""
+                      "--Boundary-00=_zmUeKB+A8hGfCVZ--";
+    msg->setContent(mail);
+    msg->parse();
+
     MessageViewer::Viewer *viewer = new MessageViewer::Viewer(0);
     vbox->addWidget(viewer);
-    //viewer->setPluginName(parser.value(QStringLiteral("headerstyleplugin")));
-    viewer->setMessage(readAndParseMail(QStringLiteral("encapsulated-with-attachment.mbox")));
+    viewer->setMessage(readAndParseMail(QStringLiteral("encapsulated-with-attachment.mbox"))/*KMime::Message::Ptr(msg)*/);
+    viewer->setPluginName(QStringLiteral("longheaderstyleplugin"));
 }
 
 TestMailDndAttachment::~TestMailDndAttachment()
@@ -48,18 +106,16 @@ TestMailDndAttachment::~TestMailDndAttachment()
 KMime::Message::Ptr TestMailDndAttachment::readAndParseMail(const QString &mailFile)
 {
     QFile file(QLatin1String(MAIL_DATA_DIR) + QLatin1Char('/') + mailFile);
-    Q_ASSERT(file.open(QIODevice::ReadOnly));
-    qDebug()<<" file"<<file.fileName();
-    const QByteArray data = KMime::CRLFtoLF(file.readAll());
-    qDebug()<<" data "<< data;
+    file.open(QIODevice::ReadOnly);
+    QByteArray ba = file.readAll();
+    qDebug() << ba;
+    const QByteArray data = ba;
     Q_ASSERT(!data.isEmpty());
     KMime::Message::Ptr msg(new KMime::Message);
     msg->setContent(data);
     msg->parse();
     return msg;
 }
-
-
 
 int main(int argc, char *argv[])
 {
