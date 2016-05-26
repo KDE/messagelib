380c166e5de4b9d6a4ffd21e82d55ff39b84c3b1
Montel Laurent <montel@kde.org>
2016-05-26 13:31:37 +0200

    Add qDebug

--- webengineviewer/src/print/printpreviewpagewidget.cpp
+++ webengineviewer/src/print/printpreviewpagewidget.cpp
@@ -125,5 +125,6 @@ void PrintPreviewPageWidget::showPage(int index)
 
 void PrintPreviewPageWidget::print(const QList<int> &page)
 {
+    qDebug()<<" void PrintPreviewPageWidget::print(const QList<int> &page) not implemented";
     //TODO
 }
