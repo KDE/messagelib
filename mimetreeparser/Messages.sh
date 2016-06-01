d8c18a12adcf290e6635a94ff252325d2d1f7313
Montel Laurent <montel@kde.org>
2016-06-01 08:24:38 +0200

    Use correct po file

--- mimetreeparser/Messages.sh
+++ mimetreeparser/Messages.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 $EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find . -name \*.html` >> html.cpp
-$XGETTEXT `find . -name '*.cpp' -o -name '*.h' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libmessageviewer.pot
+$XGETTEXT `find . -name '*.cpp' -o -name '*.h' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libmimetreeparser.pot
 rm -f html.cpp
