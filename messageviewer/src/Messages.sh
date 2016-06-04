9a89e1b759ef7925f4f441a781f9b29567db13b1
Sandro Knauß <sknauss@kde.org>
2016-06-04 17:30:20 +0200

    move i18n from mimetreeparser -> messageviewer

--- messageviewer/src/Messages.sh
+++ messageviewer/src/Messages.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
+$EXTRACT_GRANTLEE_TEMPLATE_STRINGS `find . -name \*.html` >> html.cpp
 $EXTRACTRC `find . -name '*.ui' -or -name '*.rc' -or -name '*.kcfg' -or -name '*.kcfg.cmake'` >> rc.cpp || exit 11
 $XGETTEXT `find . -name '*.cpp' -o -name '*.h' | grep -v '/tests/' | grep -v '/autotests/'` -o $podir/libmessageviewer.pot
 rm -f rc.cpp
