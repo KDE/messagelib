0962e61dc23155bd67a277e692409beeb96412c2
Montel Laurent <montel@kde.org>
2016-06-05 09:15:08 +0200

    Remove not necessary includes

--- mimetreeparser/src/viewer/nodehelper.cpp
+++ mimetreeparser/src/viewer/nodehelper.cpp
@@ -23,7 +23,6 @@
 #include "interfaces/bodypart.h"
 #include "temporaryfile/attachmenttemporaryfilesdirs.h"
 
-#include <MessageCore/NodeHelper>
 
 #include <KMime/Content>
 #include <KMime/Message>
