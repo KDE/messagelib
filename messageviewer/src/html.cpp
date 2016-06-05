856c16b46b3ecd23153f6eb6bee3c34aef9dd0e2
l10n daemon script <scripty@kde.org>
2016-06-05 07:42:06 +0000

    SVN_SILENT made messages (after extraction)

---
+++ messageviewer/src/html.cpp
@@ -0,0 +1,121 @@
+// i18n: file: ./messagepartthemes/default/templates/certmessagepart.html:2
+gettext("Sorry, certificate could not be imported.");
+// i18n: file: ./messagepartthemes/default/templates/certmessagepart.html:4
+gettext("Reason: %1");
+// i18n: file: ./messagepartthemes/default/templates/certmessagepart.html:7
+gettext("Sorry, no certificates were found in this message.");
+// i18n: file: ./messagepartthemes/default/templates/certmessagepart.html:9
+gettext("Certificate import status:");
+// i18n: file: ./messagepartthemes/default/templates/certmessagepart.html:13
+ngettext("1 new certificate was imported.", "%1 new certificates were imported.");
+// i18n: file: ./messagepartthemes/default/templates/certmessagepart.html:16
+ngettext("1 certificate was unchanged.", "%1 certificates were unchanged.");
+// i18n: file: ./messagepartthemes/default/templates/certmessagepart.html:19
+ngettext("1 new secret key was imported.", "%1 new secret keys were imported.");
+// i18n: file: ./messagepartthemes/default/templates/certmessagepart.html:22
+ngettext("1 secret key was unchanged.", "%1 secret keys were unchanged.");
+// i18n: file: ./messagepartthemes/default/templates/certmessagepart.html:28
+gettext("Sorry, no details on certificate import available.");
+// i18n: file: ./messagepartthemes/default/templates/certmessagepart.html:30
+gettext("Certificate import details:");
+// i18n: file: ./messagepartthemes/default/templates/certmessagepart.html:34
+pgettext("Certificate import failed.", "Failed: %1 (%2)");
+// i18n: file: ./messagepartthemes/default/templates/encryptedmessagepart.html:3
+gettext("This message is encrypted.");
+// i18n: file: ./messagepartthemes/default/templates/encryptedmessagepart.html:6
+gettext("Decrypt Message");
+// i18n: file: ./messagepartthemes/default/templates/encryptedmessagepart.html:13
+gettext("Please wait while the message is being decrypted...");
+// i18n: file: ./messagepartthemes/default/templates/encryptedmessagepart.html:15
+gettext("Encrypted message");
+// i18n: file: ./messagepartthemes/default/templates/encryptedmessagepart.html:17
+gettext("Encrypted message (decryption not possible)");
+// i18n: file: ./messagepartthemes/default/templates/encryptedmessagepart.html:20
+gettext("Reason: ");
+// i18n: file: ./messagepartthemes/default/templates/encryptedmessagepart.html:32
+gettext("No secret key found to encrypt the message. It is encrypted for following keys:");
+// i18n: file: ./messagepartthemes/default/templates/encryptedmessagepart.html:38
+gettext("Could not decrypt the data.");
+// i18n: file: ./messagepartthemes/default/templates/encryptedmessagepart.html:45
+gettext("End of encrypted message");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:5
+gettext("Please wait while the signature is being verified...");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:15
+gettext("Not enough information to check signature. %1");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:19
+gettext("Message was signed by %1.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:21
+gettext("Message was signed with unknown key.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:27
+gettext("Message was signed with key %1.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:30
+gettext("Message was signed by %3 on %1 with key %2");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:35
+gettext("Message was signed with key %1.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:38
+gettext("Message was signed on %1 with key %2.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:44
+gettext("Message was signed by %2 with key %1.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:47
+gettext("Message was signed with key %1.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:56
+gettext("Status: ");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:70
+gettext("Hide Details");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:82
+gettext("Message was signed on %1 with unknown key %2.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:85
+gettext("Message was signed with unknown key %1.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:89
+gettext("Message was signed with unknown key.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:92
+gettext("The validity of the signature cannot be verified.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:95
+gettext("Status: ");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:102
+gettext("Message was signed by %2 (Key ID: %1).");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:105
+gettext("Message was signed by %1.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:109
+gettext("The signature is valid, but the key's validity is unknown.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:111
+gettext("The signature is valid and the key is marginally trusted.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:113
+gettext("The signature is valid and the key is fully trusted.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:115
+gettext("The signature is valid and the key is ultimately trusted.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:117
+gettext("The signature is valid, but the key is untrusted.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:121
+gettext("Message was signed by %2 (Key ID: %1).");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:124
+gettext("Message was signed by %1.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:127
+gettext("Warning: The signature is bad.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:131
+gettext("Hide Details");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:138
+gettext("Invalid signature.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:140
+gettext("Not enough information to check signature validity.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:143
+gettext("Signed by <a href="mailto:%1">%2</a>.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:146
+gettext("Signature is valid.");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:149
+gettext("Unknown signature state");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:153
+gettext("Show Details");
+// i18n: file: ./messagepartthemes/default/templates/signedmessagepart.html:165
+gettext("End of signed message");
+// i18n: file: ./messagepartthemes/default/templates/htmlmessagepart.html:4
+gettext("<b>Note:</b> This is an HTML message. For security reasons, only the raw HTML code is shown. If you trust the sender of this message then you can activate formatted HTML display for this message <a href="kmail:showHTML">by clicking here</a>.");
+// i18n: file: ./messagepartthemes/default/templates/htmlmessagepart.html:6
+gettext("<b>Note:</b> This HTML message may contain external references to images etc. For security/privacy reasons external references are not loaded. If you trust the sender of this message then you can load the external references for this message <a href="kmail:loadExternal">by clicking here</a>.");
+// i18n: file: ./messagepartthemes/default/templates/encapsulatedrfc822messagepart.html:5
+gettext("Encapsulated message");
+// i18n: file: ./messagepartthemes/default/templates/encapsulatedrfc822messagepart.html:7
+gettext("Encapsulated message");
+// i18n: file: ./messagepartthemes/default/templates/encapsulatedrfc822messagepart.html:19
+gettext("End of encapsulated message");
+
