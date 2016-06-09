97724a3b6fe2c8050ca76bdd13f2b7f98a33967f
Montel Laurent <montel@kde.org>
2016-06-09 07:31:00 +0200

    Use checkable

--- webengineviewer/src/print/printselectpagewidget.cpp
+++ webengineviewer/src/print/printselectpagewidget.cpp
@@ -44,7 +44,8 @@ void PrintSelectPageWidget::setPages(int page)
 {
     for (int i = 0; i < page; ++i) {
         QListWidgetItem *item = new QListWidgetItem(i18n("Page %1", (i+1)), mListPage);
-        item->setFlags(item->flags() | Qt::ItemIsSelectable);
+        item->setFlags(item->flags() | Qt::ItemIsUserCheckable);
+        item->setCheckState(Qt::Unchecked);
         item->setData(PageIndex, i);
     }
 }
