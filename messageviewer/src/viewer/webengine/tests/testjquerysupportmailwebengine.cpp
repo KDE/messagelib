3147a9cce4681404b17cc37343dc6f88b4f51648
Montel Laurent <montel@kde.org>
2016-07-02 14:59:44 +0200

    we need a test for jquery + emails

---
+++ messageviewer/src/viewer/webengine/tests/testjquerysupportmailwebengine.cpp
@@ -0,0 +1,72 @@
+/*
+   Copyright (C) 2016 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "testjquerysupportmailwebengine.h"
+#include <QHBoxLayout>
+#include <QApplication>
+#include <QWebEngineView>
+#include <WebEngineViewer/WebEnginePage>
+#include <QDebug>
+#include <webengineview.h>
+#include <QPushButton>
+#include <QMessageBox>
+#include <QTextEdit>
+
+TestJQuerySupportMailWebEngine::TestJQuerySupportMailWebEngine(QWidget *parent)
+    : QWidget(parent)
+{
+    QVBoxLayout *vboxLayout = new QVBoxLayout(this);
+    //true => use JQuery
+    pageView = new WebEngineViewer::WebEngineView(true, this);
+    vboxLayout->addWidget(pageView);
+
+    mEditor = new QTextEdit(this);
+    mEditor->setAcceptRichText(false);
+    mEditor->setPlainText(QStringLiteral("qt.jQuery('img').each( function () { qt.jQuery(this).css('-webkit-transition', '-webkit-transform 2s'); qt.jQuery(this).css('-webkit-transform', 'rotate(180deg)') } ); undefined"));
+    vboxLayout->addWidget(mEditor);
+
+    QPushButton *executeQuery = new QPushButton(QStringLiteral("Execute Query"), this);
+    connect(executeQuery, &QPushButton::clicked, this, &TestJQuerySupportMailWebEngine::slotExecuteQuery);
+    vboxLayout->addWidget(executeQuery);
+
+    pageView->load(QUrl(QStringLiteral("http://www.kde.org")));
+}
+
+TestJQuerySupportMailWebEngine::~TestJQuerySupportMailWebEngine()
+{
+
+}
+
+void TestJQuerySupportMailWebEngine::slotExecuteQuery()
+{
+    const QString code = mEditor->toPlainText();
+    if (!code.isEmpty()) {
+        pageView->page()->runJavaScript(code);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    QApplication app(argc, argv);
+    app.setAttribute(Qt::AA_UseHighDpiPixmaps, true);
+    TestJQuerySupportMailWebEngine *testWebEngine = new TestJQuerySupportMailWebEngine;
+    testWebEngine->show();
+    const int ret = app.exec();
+    return ret;
+}
