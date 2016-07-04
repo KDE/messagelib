d2e6330a60087162374a0ba5e386e9f21fb6929b
Montel Laurent <montel@kde.org>
2016-07-04 13:42:49 +0200

    Add autotest for mailwebenginescript

---
+++ messageviewer/src/viewer/webengine/autotests/mailwebenginescripttest.cpp
@@ -0,0 +1,16 @@
+#include "mailwebenginescripttest.h"
+
+#include <QTest>
+
+MailWebEngineScriptTest::MailWebEngineScriptTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+MailWebEngineScriptTest::~MailWebEngineScriptTest()
+{
+
+}
+
+QTEST_MAIN(MailWebEngineScriptTest)
