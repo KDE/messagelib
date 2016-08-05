0e4ac4682e6c94ad4af8f03061870b2042e30f38
Montel Laurent <montel@kde.org>
2016-08-05 07:56:04 +0200

    Remove duplicate margin

--- messagelist/src/utils/configureaggregationsdialog.cpp
+++ messagelist/src/utils/configureaggregationsdialog.cpp
@@ -120,6 +120,7 @@ ConfigureAggregationsDialog::ConfigureAggregationsDialog(QWidget *parent)
     mainLayout->addWidget(buttonBox);
 
     QGridLayout *g = new QGridLayout(base);
+    g->setMargin(0);
 
     d->mAggregationList = new AggregationListWidget(base);
     d->mAggregationList->setSelectionMode(QAbstractItemView::ExtendedSelection);
