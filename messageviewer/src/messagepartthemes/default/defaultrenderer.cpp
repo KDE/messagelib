c1d1b7fb6ebeb6c060b172567aa6df9104a6aeed
Montel Laurent <montel@kde.org>
2016-10-24 21:03:50 +0200

    Fix Bug 369072 - Regression kMail 5.3.x: Plain-text quoting display is messed up
    FIXED-IN: 3.4.0
    BUG: 369072

--- messageviewer/src/messagepartthemes/default/defaultrenderer.cpp
+++ messageviewer/src/messagepartthemes/default/defaultrenderer.cpp
@@ -772,7 +772,6 @@ public:
                                            .arg(-1)
                                            .arg(mExpandIconPath);
                                 htmlStr += QLatin1String("</div><br/>");
-                                htmlStr += quoteEnd;
                             }
                         } else {
                             htmlStr += QLatin1String("<div class=\"quotelevelmark\" >");
