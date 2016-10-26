1518beca32293b8518008532102f25f862321479
Montel Laurent <montel@kde.org>
2016-10-26 23:01:30 +0200

    Add check url job

---
+++ messageviewer/src/checkphishingurl/checkphishingurljob.h
@@ -0,0 +1,44 @@
+/*
+   Copyright (C) 2016 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef CHECKPHISHINGURLJOB_H
+#define CHECKPHISHINGURLJOB_H
+
+#include <QObject>
+#include <QUrl>
+
+namespace MessageViewer {
+class CheckPhishingUrlJob : public QObject
+{
+    Q_OBJECT
+public:
+    explicit CheckPhishingUrlJob(QObject *parent = Q_NULLPTR);
+    ~CheckPhishingUrlJob();
+
+    void setUrl(const QUrl &url);
+
+    void start();
+    bool canStart() const;
+
+private:
+    QUrl mUrl;
+};
+}
+
+#endif // CHECKPHISHINGURLJOB_H
