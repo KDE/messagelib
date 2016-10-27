70173a8245c6f0401619f5be9cc4770451feeb21
Montel Laurent <montel@kde.org>
2016-10-27 21:02:35 +0200

    Fix Bug 368498 - Kmail does not open links with target="_blank"
    FIXED-IN: 5.4.0
    BUG: 368498

---
+++ webengineviewer/src/webenginenavigationrequestinterceptor.h
@@ -0,0 +1,43 @@
+/*
+   Copyright (C) 2016 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef WEBENGINENAVIGATIONREQUESTINTERCEPTOR_H
+#define WEBENGINENAVIGATIONREQUESTINTERCEPTOR_H
+
+#include <QWebEnginePage>
+
+namespace WebEngineViewer
+{
+class WebEnginePage;
+class WebEngineNavigationRequestInterceptor : public QWebEnginePage
+{
+    Q_OBJECT
+public:
+    explicit WebEngineNavigationRequestInterceptor(QWebEnginePage *page);
+    ~WebEngineNavigationRequestInterceptor();
+
+protected:
+    bool acceptNavigationRequest(const QUrl &url, NavigationType type, bool isMainFrame) Q_DECL_OVERRIDE;
+
+private:
+    QWebEnginePage *mTargetPage;
+};
+}
+
+#endif // WEBENGINENAVIGATIONREQUESTINTERCEPTOR_H
