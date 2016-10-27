b9b37534c27266092b92e220b33bcaab9d068c89
Montel Laurent <montel@kde.org>
2016-10-27 07:36:41 +0200

    Add test application

---
+++ messageviewer/src/checkphishingurl/tests/checkphishingurlgui.cpp
@@ -0,0 +1,62 @@
+/*
+   Copyright (C) 2016 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "checkphishingurlgui.h"
+
+#include <QApplication>
+#include <QStandardPaths>
+#include <QLineEdit>
+#include <QTextEdit>
+#include <QLabel>
+#include <QVBoxLayout>
+
+CheckPhishingUrlGui::CheckPhishingUrlGui(QWidget *parent)
+    : QWidget(parent)
+{
+    QVBoxLayout *layout = new QVBoxLayout(this);
+
+    QHBoxLayout *checkUrlLayout = new QHBoxLayout;
+    layout->addLayout(checkUrlLayout);
+    QLabel *lab = new QLabel(QStringLiteral("Url to Check:"), this);
+    checkUrlLayout->addWidget(lab);
+    mCheckUrlLineEdit = new QLineEdit(this);
+    checkUrlLayout->addWidget(mCheckUrlLineEdit);
+
+    mResult = new QTextEdit(this);
+    mResult->setReadOnly(true);
+    mResult->setAcceptRichText(false);
+    layout->addWidget(mResult);
+}
+
+CheckPhishingUrlGui::~CheckPhishingUrlGui()
+{
+
+}
+
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+    QStandardPaths::setTestModeEnabled(true);
+    CheckPhishingUrlGui *w = new CheckPhishingUrlGui;
+
+    w->show();
+    app.exec();
+    delete w;
+    return 0;
+}
