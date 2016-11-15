40d0a5adf74717a5244b33d416bbf1e618d72a2e
Montel Laurent <montel@kde.org>
2016-11-15 22:42:02 +0100

    Add class to check url from local database

---
+++ webengineviewer/src/checkphishingurl/checkphishingurlfromlocaldatabasejob.cpp
@@ -0,0 +1,59 @@
+/*
+   Copyright (C) 2016 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "checkphishingurlfromlocaldatabasejob.h"
+
+using namespace WebEngineViewer;
+
+CheckPhishingUrlFromLocalDataBaseJob::CheckPhishingUrlFromLocalDataBaseJob(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+WebEngineViewer::CheckPhishingUrlFromLocalDataBaseJob::~CheckPhishingUrlFromLocalDataBaseJob()
+{
+
+}
+
+void CheckPhishingUrlFromLocalDataBaseJob::setCheckPhisingUrl(const QUrl &url)
+{
+    mUrl = url;
+}
+
+void CheckPhishingUrlFromLocalDataBaseJob::start()
+{
+    if (mUrl.isValid()) {
+        //TODO
+        deleteLater();
+    } else {
+        //TODO
+    }
+}
+
+bool CheckPhishingUrlFromLocalDataBaseJob::canStart() const
+{
+    return mUrl.isValid();
+}
+
+QString CheckPhishingUrlFromLocalDataBaseJob::createHash()
+{
+    //TODO
+    return {};
+}
