1ad5418bc0a9fff196b9e44beb51efd9878a621f
Montel Laurent <montel@kde.org>
2016-12-12 13:31:18 +0100

    We need a thread here

---
+++ webengineviewer/src/checkphishingurl/downloadlocaldatabasethread.h
@@ -0,0 +1,37 @@
+/*
+   Copyright (C) 2016 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef DOWNLOADLOCALDATABASETHREAD_H
+#define DOWNLOADLOCALDATABASETHREAD_H
+
+#include <QThread>
+namespace WebEngineViewer
+{
+class DownloadLocalDatabaseThread : public QThread
+{
+    Q_OBJECT
+public:
+    explicit DownloadLocalDatabaseThread(QObject *parent = Q_NULLPTR);
+    ~DownloadLocalDatabaseThread();
+
+protected:
+    void run() Q_DECL_OVERRIDE;
+};
+}
+#endif // DOWNLOADLOCALDATABASETHREAD_H
