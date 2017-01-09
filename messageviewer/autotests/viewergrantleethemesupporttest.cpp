ad4b27c05281d66afbbafacdcd9839aeb1630450
Montel Laurent <montel@kde.org>
2017-01-09 21:33:03 +0100

    Start to create high level autotest for testing grantlee theme support

---
+++ messageviewer/autotests/viewergrantleethemesupporttest.cpp
@@ -0,0 +1,46 @@
+/*
+  Copyright (c) 2017 Montel Laurent <montel@kde.org>
+
+  This program is free software; you can redistribute it and/or modify it
+  under the terms of the GNU General Public License, version 2, as
+  published by the Free Software Foundation.
+
+  This program is distributed in the hope that it will be useful, but
+  WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  General Public License for more details.
+
+  You should have received a copy of the GNU General Public License along
+  with this program; if not, write to the Free Software Foundation, Inc.,
+  51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "viewergrantleethemesupporttest.h"
+#include <QStandardPaths>
+#include <KConfigGroup>
+#include <KSharedConfig>
+#include <QTest>
+
+ViewerGrantleeThemeSupportTest::ViewerGrantleeThemeSupportTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+ViewerGrantleeThemeSupportTest::~ViewerGrantleeThemeSupportTest()
+{
+
+}
+
+void ViewerGrantleeThemeSupportTest::initTestCase()
+{
+    QStandardPaths::setTestModeEnabled(true);
+
+    // Point the test to our dummy icon theme
+    KConfigGroup cg(KSharedConfig::openConfig(), "Icons");
+    cg.writeEntry("Theme", "dummyTheme");
+    qputenv("XDG_DATA_DIRS", GRANTLEETHEME_DATA_DIR);
+}
+
+
+QTEST_MAIN(ViewerGrantleeThemeSupportTest)
