863a7b9b28bf963af4984ae8998ae030aeb243fb
Montel Laurent <montel@kde.org>
2017-02-03 06:59:45 +0100

    Add autotest

---
+++ templateparser/autotests/templatewebengineviewtest.cpp
@@ -0,0 +1,41 @@
+/*
+   Copyright (C) 2017 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "templatewebengineviewtest.h"
+#include "templatewebengineview.h"
+#include <QTest>
+
+TemplateWebEngineViewTest::TemplateWebEngineViewTest(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+TemplateWebEngineViewTest::~TemplateWebEngineViewTest()
+{
+
+}
+
+void TemplateWebEngineViewTest::shouldHaveDefaultValue()
+{
+    TemplateParser::TemplateWebEngineView w;
+    QVERIFY(w.html().isEmpty());
+}
+
+QTEST_MAIN(TemplateWebEngineViewTest)
