a9a88314c1ec4c139105cc063c8bdd9fd891900b
Montel Laurent <montel@kde.org>
2017-03-10 08:05:22 +0100

    GIT_SILENT: fix copyright

--- webengineviewer/src/findbar/findbarwebengineview.h
+++ webengineviewer/src/findbar/findbarwebengineview.h
@@ -1,4 +1,4 @@
-/* Copyright (C) 2016 Laurent Montel <montel@kde.org>
+/* Copyright (C) 2016-2017 Laurent Montel <montel@kde.org>
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Library General Public
