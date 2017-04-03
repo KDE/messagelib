721fe8f61d8c9fe24878c7259156dfd08105c299
Montel Laurent <montel@kde.org>
2017-04-03 09:03:25 +0200

    Reduce number of toUpper calling

--- mimetreeparser/src/viewer/nodehelper.cpp
+++ mimetreeparser/src/viewer/nodehelper.cpp
@@ -757,8 +757,9 @@ QString NodeHelper::fixEncoding(const QString &encoding)
     QString returnEncoding = encoding;
     // According to http://www.iana.org/assignments/character-sets, uppercase is
     // preferred in MIME headers
-    if (returnEncoding.toUpper().contains(QStringLiteral("ISO "))) {
-        returnEncoding = returnEncoding.toUpper();
+    const QString returnEncodingToUpper = returnEncoding.toUpper();
+    if (returnEncodingToUpper.contains(QStringLiteral("ISO "))) {
+        returnEncoding = returnEncodingToUpper;
         returnEncoding.replace(QLatin1String("ISO "), QStringLiteral("ISO-"));
     }
     return returnEncoding;
