257d76080a9dee324d99d42b7c11700defcd52d6
Montel Laurent <montel@kde.org>
2017-04-27 07:44:29 +0200

    Add interface too

---
+++ messagecomposer/src/plugineditorinit/plugineditorinitinterface.cpp
@@ -0,0 +1,60 @@
+/*
+   Copyright (C) 2017 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "plugineditorinitinterface.h"
+
+using namespace MessageComposer;
+
+class MessageComposer::PluginEditorInitInterfacePrivate
+{
+public:
+    PluginEditorInitInterfacePrivate()
+        : mParentWidget(nullptr)
+    {
+
+    }
+    QWidget *mParentWidget;
+};
+
+PluginEditorInitInterface::PluginEditorInitInterface(QObject *parent)
+    : QObject(parent),
+      d(new MessageComposer::PluginEditorInitInterfacePrivate)
+{
+
+}
+
+PluginEditorInitInterface::~PluginEditorInitInterface()
+{
+    delete d;
+}
+
+void PluginEditorInitInterface::setParentWidget(QWidget *parent)
+{
+    d->mParentWidget = parent;
+}
+
+QWidget *PluginEditorInitInterface::parentWidget() const
+{
+    return d->mParentWidget;
+}
+
+void PluginEditorInitInterface::reloadConfig()
+{
+    //Reimplement it
+}
---
+++ messagecomposer/src/plugineditorinit/plugineditorinitinterface.h
@@ -0,0 +1,50 @@
+/*
+   Copyright (C) 2017 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef PLUGINEDITORINITINTERFACE_H
+#define PLUGINEDITORINITINTERFACE_H
+
+#include <QObject>
+#include "messagecomposer_export.h"
+
+namespace MessageComposer
+{
+class PluginEditorInitInterfacePrivate;
+class PluginEditorInitParams;
+class MESSAGECOMPOSER_EXPORT PluginEditorInitInterface : public QObject
+{
+    Q_OBJECT
+public:
+    explicit PluginEditorInitInterface(QObject *parent = nullptr);
+    ~PluginEditorInitInterface();
+
+    virtual bool exec() = 0;
+
+    void setParentWidget(QWidget *parent);
+    QWidget *parentWidget() const;
+
+public Q_SLOTS:
+    virtual void reloadConfig();
+
+private:
+    PluginEditorInitInterfacePrivate *const d;
+};
+}
+
+#endif // PLUGINEDITORINITINTERFACE_H
