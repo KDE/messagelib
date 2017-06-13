bdd4228e58359c6614c8add85196334e0c37d100
Montel Laurent <montel@kde.org>
2017-06-13 13:32:41 +0200

    Add interface widget

---
+++ templateparser/src/templateparseremailaddressrequesterinterfacewidget.cpp
@@ -0,0 +1,65 @@
+/*
+   Copyright (C) 2017 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#include "templateparseremailaddressrequesterinterfacewidget.h"
+#include <QHBoxLayout>
+#include <TemplateParser/TemplateParserEmailAddressRequesterBase>
+#include "templateparseremailaddressrequesterlineedit.h"
+#include <KPluginLoader>
+#include <KPluginFactory>
+
+using namespace TemplateParser;
+
+TemplateParserEmailAddressRequesterInterfaceWidget::TemplateParserEmailAddressRequesterInterfaceWidget(QWidget *parent)
+    : QWidget(parent)
+    , mTemplateParserEmailBase(nullptr)
+{
+    QHBoxLayout *mainLayout = new QHBoxLayout(this);
+    mainLayout->setObjectName(QStringLiteral("mainlayout"));
+    mainLayout->setMargin(0);
+    initializeEmailWidget();
+}
+
+void TemplateParserEmailAddressRequesterInterfaceWidget::initializeEmailWidget()
+{
+    KPluginLoader loader(QStringLiteral("templateparser/templateparseraddressrequesterplugin"));
+    KPluginFactory *factory = loader.factory();
+    if (factory) {
+        mTemplateParserEmailBase = factory->create<TemplateParser::TemplateParserEmailAddressRequesterBase>(this);
+    } else {
+        mTemplateParserEmailBase = new TemplateParser::TemplateParserEmailAddressRequesterLineEdit(this);
+    }
+    connect(mTemplateParserEmailBase, &TemplateParserEmailAddressRequesterBase::textChanged,
+            this, &TemplateParserEmailAddressRequesterInterfaceWidget::textChanged);
+}
+
+QString TemplateParserEmailAddressRequesterInterfaceWidget::text() const
+{
+    return mTemplateParserEmailBase->text();
+}
+
+void TemplateParserEmailAddressRequesterInterfaceWidget::setText(const QString &str)
+{
+    mTemplateParserEmailBase->setText(str);
+}
+
+void TemplateParserEmailAddressRequesterInterfaceWidget::clear()
+{
+    mTemplateParserEmailBase->clear();
+}
