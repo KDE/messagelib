ece5d7d4e123870e3aaaafbb12c524d2ca3a6263
Volker Krause <vkrause@kde.org>
2017-10-13 18:57:59 +0200

    Fix one more QStringLiteral lambda inside Q_FOREACH breaking the CI

--- templateparser/autotests/templateparserjobtest.cpp
+++ templateparser/autotests/templateparserjobtest.cpp
@@ -106,7 +106,8 @@ void TemplateParserJobTest::test_replyPlain_data()
     QTest::addColumn<QString>("referenceFileName");
 
     QDir dir(QStringLiteral(MAIL_DATA_DIR));
-    foreach (const QString &file, dir.entryList(QStringList(QStringLiteral("*.mbox")), QDir::Files | QDir::Readable | QDir::NoSymLinks)) {
+    const auto l = dir.entryList(QStringList(QStringLiteral("*.mbox")), QDir::Files | QDir::Readable | QDir::NoSymLinks);
+    foreach (const QString &file, l) {
         const QString expectedFile = dir.path() + QLatin1Char('/') + file + QStringLiteral(".plain.reply");
         if (!QFile::exists(expectedFile)) {
             continue;
