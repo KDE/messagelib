a7bf8026c4bdcd930d3dc78f8743ddf19e3e9166
Laurent Montel <montel@kde.org>
2018-01-03 08:09:13 +0100

    Add debug + minor optimization

--- messageviewer/src/header/headerstyle_util.cpp
+++ messageviewer/src/header/headerstyle_util.cpp
@@ -66,10 +66,12 @@ QString HeaderStyleUtil::strToHtml(const QString &str, KTextToHTML::Options flag
 QString HeaderStyleUtil::dateString(KMime::Message *message, bool printing, HeaderStyleUtilDateFormat dateFormat) const
 {
     const QDateTime dateTime = message->date()->dateTime();
-    const time_t unixTime = dateTime.toTime_t();
     if (!dateTime.isValid()) {
+        qCDebug(MESSAGEVIEWER_LOG) << "Unable to parse date";
         return i18nc("Unknown date", "Unknown");
     }
+
+    const time_t unixTime = dateTime.toTime_t();
     if (printing) {
         return KMime::DateFormatter::formatDate(KMime::DateFormatter::Localized, unixTime);
     } else {
