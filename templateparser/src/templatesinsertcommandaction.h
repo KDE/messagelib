5bbde76d85ea6a57d7b695ca8a6bd46396118325
Laurent Montel <montel@kde.org>
2018-02-04 14:53:53 +0100

    Add action element

---
+++ templateparser/src/templatesinsertcommandaction.h
@@ -0,0 +1,38 @@
+/*
+   Copyright (C) 2018 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
+#ifndef TEMPLATESINSERTCOMMANDACTION_H
+#define TEMPLATESINSERTCOMMANDACTION_H
+
+#include <QAction>
+
+#include "templateparser_export.h"
+
+namespace TemplateParser {
+class TEMPLATEPARSER_EXPORT TemplatesInsertCommandAction : public QAction
+{
+    Q_OBJECT
+public:
+    explicit TemplatesInsertCommandAction(QObject *parent = nullptr);
+    ~TemplatesInsertCommandAction();
+};
+
+}
+#endif // TEMPLATESINSERTCOMMANDACTION_H
