0720d1e9b48157b88679964c3b483d87bb47b3bb
Laurent Montel <montel@kde.org>
2018-03-16 13:59:59 +0100

    Start to create job for convertir plain text which has some template command

---
+++ templateparser/src/templateconvertcommandjob.h
@@ -0,0 +1,37 @@
+/*
+   Copyright (C) 2018 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef TEMPLATECONVERTCOMMANDJOB_H
+#define TEMPLATECONVERTCOMMANDJOB_H
+
+#include <QObject>
+#include "templateparser_export.h"
+
+namespace TemplateParser {
+class TEMPLATEPARSER_EXPORT TemplateConvertCommandJob : public QObject
+{
+    Q_OBJECT
+public:
+    explicit TemplateConvertCommandJob(QObject *parent = nullptr);
+    ~TemplateConvertCommandJob();
+    void start();
+};
+}
+
+#endif // TEMPLATECONVERTCOMMANDJOB_H
