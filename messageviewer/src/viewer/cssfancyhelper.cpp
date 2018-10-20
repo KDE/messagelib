388d0ec8f91ff414dcbeaafde29568baf9f57493
Laurent Montel <montel@kde.org>
2018-10-20 22:16:39 +0200

    Start to create specific class

---
+++ messageviewer/src/viewer/cssfancyhelper.cpp
@@ -0,0 +1,52 @@
+/*
+  Copyright (c) 2018 Montel Laurent <montel@kde.org>
+
+  This library is free software; you can redistribute it and/or modify it
+  under the terms of the GNU Library General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or (at your
+  option) any later version.
+
+  This library is distributed in the hope that it will be useful, but WITHOUT
+  ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+  FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Library General Public
+  License for more details.
+
+  You should have received a copy of the GNU Library General Public License
+  along with this library; see the file COPYING.LIB.  If not, write to the
+  Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+  02110-1301, USA.
+
+*/
+
+#include "cssfancyhelper.h"
+
+using namespace MessageViewer;
+
+CSSFancyHelper::CSSFancyHelper(const QPaintDevice *pd)
+    : CSSHelper(pd)
+{
+
+}
+
+CSSFancyHelper::~CSSFancyHelper()
+{
+
+}
+
+QString MessageViewer::CSSFancyHelper::extraScreenCss() const
+{
+    //TODO
+    return {};
+}
+
+QString MessageViewer::CSSFancyHelper::extraPrintCss() const
+{
+    //TODO
+    return {};
+}
+
+QString MessageViewer::CSSFancyHelper::extraCommonCss() const
+{
+    //TODO
+    return {};
+}
