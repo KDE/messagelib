7b193ade213a38e57e11c9f32845fe9c11f9eb1d
Laurent Montel <montel@kde.org>
2018-12-10 13:51:22 +0100

    GIT_SILENT: Use https here too

--- messagecore/src/utils/stringutil.h
+++ messagecore/src/utils/stringutil.h
@@ -14,7 +14,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 #ifndef MESSAGECORE_STRINGUTIL_H
 #define MESSAGECORE_STRINGUTIL_H
--- messagelist/src/utils/themecombobox_p.h
+++ messagelist/src/utils/themecombobox_p.h
@@ -14,7 +14,7 @@
    GNU General Public License for more details.
 
    You should have received a copy of the GNU General Public License
-   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+   along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 #ifndef MESSAGELIST_UTILS_THEMECOMBOBOX_P_H
 #define MESSAGELIST_UTILS_THEMECOMBOBOX_P_H
