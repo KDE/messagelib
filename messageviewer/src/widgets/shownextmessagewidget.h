df67b3efaae4feb63ae20091528f5ad65800d24f
Laurent Montel <montel@kde.org>
2019-05-10 08:10:27 +0200

    Start to create widget for showing several mail from mbox

---
+++ messageviewer/src/widgets/shownextmessagewidget.h
@@ -0,0 +1,36 @@
+/*
+   Copyright (C) 2019 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+
+#ifndef SHOWNEXTMESSAGEWIDGET_H
+#define SHOWNEXTMESSAGEWIDGET_H
+
+#include <QWidget>
+#include "messageviewer_private_export.h"
+namespace MessageViewer {
+class MESSAGEVIEWER_TESTS_EXPORT ShowNextMessageWidget : public QWidget
+{
+    Q_OBJECT
+public:
+    explicit ShowNextMessageWidget(QWidget *parent = nullptr);
+    ~ShowNextMessageWidget();
+};
+}
+
+#endif // SHOWNEXTMESSAGEWIDGET_H
