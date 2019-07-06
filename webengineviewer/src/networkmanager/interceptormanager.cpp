b3d4de670ad6b8428c44fc04b9a931677e881e67
Tobias C. Berner <tcberner@FreeBSD.org>
2019-07-06 17:02:38 +0200

    Use QTWEBENGINE_VERSION not QT_VERSION for WebEngine feature detection
    Summary:
    FreeBSD's Qt5WebEngine version is not in sync with the Qt5-version.
    Therefore check the version we actually care about here.
    
    Reviewers: #automotive, mlaurent, vkrause
    
    Reviewed By: #automotive, vkrause
    
    Subscribers: kde-pim
    
    Tags: #kde_pim
    
    Differential Revision: https://phabricator.kde.org/D22300

--- webengineviewer/src/networkmanager/interceptormanager.cpp
+++ webengineviewer/src/networkmanager/interceptormanager.cpp
@@ -47,7 +47,7 @@ InterceptorManager::InterceptorManager(QWebEngineView *webEngine, KActionCollect
     for (WebEngineViewer::NetworkPluginUrlInterceptorInterface *interface : d->mManager->interfaceList()) {
         d->mNetworkUrlInterceptor->addInterceptor(interface);
     }
-#if QT_VERSION < QT_VERSION_CHECK(5, 13, 0)
+#if QTWEBENGINE_VERSION < QT_VERSION_CHECK(5, 13, 0)
     webEngine->page()->profile()->setRequestInterceptor(d->mNetworkUrlInterceptor);
 #else
     webEngine->page()->profile()->setUrlRequestInterceptor(d->mNetworkUrlInterceptor);
