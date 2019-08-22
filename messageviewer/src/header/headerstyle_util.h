5715f0c4801d20f67f03b02a413cd5582f16838d
Sandro Knauß <sknauss@kde.org>
2019-08-23 00:11:32 +0200

    Mark first functions as deprecated as we want to force people to use the new header interface via NodeHelper.

--- messageviewer/src/header/headerstyle_util.h
+++ messageviewer/src/header/headerstyle_util.h
@@ -74,9 +74,10 @@ public:
     static Q_REQUIRED_RESULT QString dateStr(const QDateTime &dateTime);
 
     static Q_REQUIRED_RESULT QString dateShortStr(const QDateTime &dateTime);
-    static Q_REQUIRED_RESULT QSharedPointer<KMime::Headers::Generics::MailboxList> resentFromList(KMime::Message *message);
 
-    static Q_REQUIRED_RESULT QSharedPointer<KMime::Headers::Generics::MailboxList> resentToList(KMime::Message *message);
+    MESSAGEVIEWER_DEPRECATED static Q_REQUIRED_RESULT QSharedPointer<KMime::Headers::Generics::MailboxList> resentFromList(KMime::Message *message);
+
+    MESSAGEVIEWER_DEPRECATED static Q_REQUIRED_RESULT QSharedPointer<KMime::Headers::Generics::MailboxList> resentToList(KMime::Message *message);
 
     Q_REQUIRED_RESULT xfaceSettings xface(const HeaderStyle *style, KMime::Message *message) const;
 private:
