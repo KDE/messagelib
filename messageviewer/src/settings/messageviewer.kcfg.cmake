b04cfeb0189758d3b352e6f6787e2f6bf20329dd
Luigi Toscano <luigi.toscano@tiscali.it>
2019-09-09 11:21:34 +0200

    Improve a few user-visible strings
    Discussed here:
    https://mail.kde.org/pipermail/kde-pim/2019-August/042486.html

--- messageviewer/src/settings/messageviewer.kcfg.cmake
+++ messageviewer/src/settings/messageviewer.kcfg.cmake
@@ -147,7 +147,7 @@ xsi:schemaLocation="http://www.kde.org/standards/kcfg/1.0 http://www.kde.org/sta
       <default></default>
     </entry>
     <entry name="MailTrackingUrlEnabled" type="Bool">
-      <label>Enable Mail Tracking Url</label>
+      <label>Scan emails for tracking URLs</label>
       <default>true</default>
     </entry>    
   </group>
