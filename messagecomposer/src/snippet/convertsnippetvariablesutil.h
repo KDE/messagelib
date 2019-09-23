104518e4622f4af1e2ea0b5524029ea5a53e50a5
Laurent Montel <montel@kde.org>
2019-09-23 08:18:48 +0200

    Add util class

---
+++ messagecomposer/src/snippet/convertsnippetvariablesutil.h
@@ -0,0 +1,30 @@
+/*
+   Copyright (C) 2019 Laurent Montel <montel@kde.org>
+
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Library General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Library General Public License for more details.
+
+   You should have received a copy of the GNU Library General Public License
+   along with this library; see the file COPYING.LIB.  If not, write to
+   the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+   Boston, MA 02110-1301, USA.
+*/
+
+#ifndef CONVERTSNIPPETVARIABLESUTIL_H
+#define CONVERTSNIPPETVARIABLESUTIL_H
+#include "messagecomposer_export.h"
+#include <QString>
+
+namespace MessageComposer {
+namespace ConvertSnippetVariablesUtil {
+Q_REQUIRED_RESULT MESSAGECOMPOSER_EXPORT QString snippetVariableFromEnum();
+}
+}
+#endif // CONVERTSNIPPETVARIABLESUTIL_H
