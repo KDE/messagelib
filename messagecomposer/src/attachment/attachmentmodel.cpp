11a72f57d1b82703ec4d2435994306fc785a93d2
Laurent Montel <montel@kde.org>
2019-09-30 07:47:33 +0200

    Don't attach twice files

--- messagecomposer/src/attachment/attachmentmodel.cpp
+++ messagecomposer/src/attachment/attachmentmodel.cpp
@@ -372,6 +372,11 @@ bool AttachmentModel::setData(const QModelIndex &index, const QVariant &value, i
 bool AttachmentModel::addAttachment(const AttachmentPart::Ptr &part)
 {
     Q_ASSERT(!d->parts.contains(part));
+    for (const AttachmentPart::Ptr &partElement : qAsConst(d->parts)) {
+        if (partElement->url() == part->url()) {
+            return true;
+        }
+    }
 
     beginInsertRows(QModelIndex(), rowCount(), rowCount());
     d->parts.append(part);
