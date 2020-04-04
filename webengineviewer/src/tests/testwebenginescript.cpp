855b291a7bfe03531bedb6c1c9fb0c8dc9858b58
Laurent Montel <montel@kde.org>
2020-04-04 16:26:42 +0200

    Fix compilation with more recent Qt 5.15

--- webengineviewer/src/tests/testwebenginescript.cpp
+++ webengineviewer/src/tests/testwebenginescript.cpp
@@ -129,11 +129,7 @@ TestScriptWidget::TestScriptWidget(QWidget *parent)
     mScriptCombo = new QComboBox;
     vScriptLayout->addWidget(mScriptCombo);
     fillScriptCombo(mScriptCombo);
-#if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
     connect(mScriptCombo, qOverload<int>(&QComboBox::currentIndexChanged), this, &TestScriptWidget::slotCurrentIndexChanged);
-#else
-    connect(mScriptCombo, qOverload<int, const QString &>(&QComboBox::currentIndexChanged), this, &TestScriptWidget::slotCurrentIndexChanged);
-#endif
 
 
     mScriptEdit = new QTextEdit;
