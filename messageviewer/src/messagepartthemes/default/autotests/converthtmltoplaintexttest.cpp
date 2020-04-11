b0021079b91018d026dc0e5e5756b293151913e4
Laurent Montel <montel@kde.org>
2020-04-11 15:25:11 +0200

    Add QStringLiteral

--- messageviewer/src/messagepartthemes/default/autotests/converthtmltoplaintexttest.cpp
+++ messageviewer/src/messagepartthemes/default/autotests/converthtmltoplaintexttest.cpp
@@ -58,18 +58,18 @@ void ConvertHtmlToPlainTextTest::shouldConvertToPlainText_data()
 {
     QTest::addColumn<QString>("inputText");
     QTest::addColumn<QString>("convertedText");
-    QTest::newRow("plainText") << "foo" << "foo\n";
-    QTest::newRow("htmlText") << "<html><body>Hi! This is a KDE test</body></html>"
-                              << "Hi! This is a KDE test\n";
+    QTest::newRow("plainText") << QStringLiteral("foo") << QStringLiteral("foo\n");
+    QTest::newRow("htmlText") << QStringLiteral("<html><body>Hi! This is a KDE test</body></html>")
+                              << QStringLiteral("Hi! This is a KDE test\n");
     QTest::newRow("htmlTextWithBold")
-        << "<html><body><b>Hi!</b> This is a KDE test</body></html>"
-        << "*Hi!* This is a KDE test\n";
+        << QStringLiteral("<html><body><b>Hi!</b> This is a KDE test</body></html>")
+        << QStringLiteral("*Hi!* This is a KDE test\n");
     QTest::newRow("htmlTextWithH1")
-        << "<html><body><h1>Hi!</h1> This is a KDE test</body></html>"
-        << "*Hi!*\nThis is a KDE test\n";
+        << QStringLiteral("<html><body><h1>Hi!</h1> This is a KDE test</body></html>")
+        << QStringLiteral("*Hi!*\nThis is a KDE test\n");
     QTest::newRow("htmlTextWithUnderLine")
-        << "<html><body><u>Hi!</u> This is a KDE test</body></html>"
-        << "_Hi!_ This is a KDE test\n";
+        << QStringLiteral("<html><body><u>Hi!</u> This is a KDE test</body></html>")
+        << QStringLiteral("_Hi!_ This is a KDE test\n");
 }
 
 void ConvertHtmlToPlainTextTest::shouldConvertToPlainText()
