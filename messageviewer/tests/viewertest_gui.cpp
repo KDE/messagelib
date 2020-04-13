e10a15fc85f9da30362d3f529cab4c38fd8b5734
Laurent Montel <montel@kde.org>
2020-04-13 21:37:10 +0200

    Resize viewer

--- messageviewer/tests/viewertest_gui.cpp
+++ messageviewer/tests/viewertest_gui.cpp
@@ -96,6 +96,7 @@ int main(int argc, char **argv)
     if (parser.isSet(developerToolsOption)) {
         viewer->showDevelopmentTools();
     }
+    viewer->resize(600, 400);
     viewer->show();
 
     const int ret = app.exec();
