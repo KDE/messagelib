001d6afe5e06e9340c6dbab4ec47bdb1ca3d2174
Laurent Montel <montel@kde.org>
2020-04-16 15:49:33 +0200

    Fix compile on qt5.12

--- messageviewer/src/utils/messageviewerutil.cpp
+++ messageviewer/src/utils/messageviewerutil.cpp
@@ -695,7 +695,7 @@ QString Util::parseBodyStyle(const QString &style)
         const int indexEnd = style.indexOf(QLatin1Char('"'), indexStyle + 7);
         if (indexEnd != -1) {
             const QStringRef styleStr = style.midRef(indexStyle + 7, indexEnd - (indexStyle + 7));
-            const auto lstStyle = styleStr.split(QLatin1Char(';'), Qt::SkipEmptyParts);
+            const auto lstStyle = styleStr.split(QLatin1Char(';'), QString::SkipEmptyParts);
             QStringList lst;
             for (const auto &style : lstStyle) {
                 if (style.trimmed().contains(QLatin1String("white-space"))) {
