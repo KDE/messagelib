3675507317e0974c1a72b6ab09ab9ab598709c99
Laurent Montel <montel@kde.org>
2020-12-22 07:47:11 +0100

    Start to implement Bug 430678 - Allow to only enable specific external images
    CCBUG: 430678

---
+++ messageviewer/src/remote-content/remotecontentmanager.cpp
@@ -0,0 +1,18 @@
+/*
+   SPDX-FileCopyrightText: 2020 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "remotecontentmanager.h"
+using namespace MessageViewer;
+RemoteContentManager::RemoteContentManager(QObject *parent)
+    : QObject(parent)
+{
+
+}
+
+RemoteContentManager::~RemoteContentManager()
+{
+
+}
---
+++ messageviewer/src/remote-content/remotecontentmanager.h
@@ -0,0 +1,23 @@
+/*
+   SPDX-FileCopyrightText: 2020 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+
+#ifndef REMOTECONTENTMANAGER_H
+#define REMOTECONTENTMANAGER_H
+
+#include "messageviewer_private_export.h"
+#include <QObject>
+namespace MessageViewer {
+class MESSAGEVIEWER_TESTS_EXPORT RemoteContentManager : public QObject
+{
+    Q_OBJECT
+public:
+    explicit RemoteContentManager(QObject *parent = nullptr);
+    ~RemoteContentManager() override;
+};
+}
+
+#endif
