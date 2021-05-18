32d679a1c0e1c67f7f615b52b76deacddb8ca2d8
Laurent Montel <montel@kde.org>
2021-05-18 07:52:19 +0200

    Add autotests

---
+++ messagelist/src/core/widgets/autotests/configurefilterswidgettest.cpp
@@ -0,0 +1,26 @@
+/*
+  SPDX-FileCopyrightText: 2021 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "configurefilterswidgettest.h"
+#include "core/widgets/configurefilterswidget.h"
+#include <QListWidget>
+#include <QTest>
+#include <QVBoxLayout>
+QTEST_MAIN(ConfigureFiltersWidgetTest)
+ConfigureFiltersWidgetTest::ConfigureFiltersWidgetTest(QObject *parent)
+    : QObject(parent)
+{
+}
+
+void ConfigureFiltersWidgetTest::shouldHaveDefaultValues()
+{
+    MessageList::Core::ConfigureFiltersWidget w;
+    auto mainLayout = w.findChild<QVBoxLayout *>(QStringLiteral("mainLayout"));
+    QVERIFY(mainLayout);
+
+    auto mListFiltersWidget = w.findChild<QListWidget *>(QStringLiteral("mListFiltersWidget"));
+    QVERIFY(mListFiltersWidget);
+}
