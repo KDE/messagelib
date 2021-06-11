41a3280f9d2474e36982b5235e75b6a4df791654
Laurent Montel <montel@kde.org>
2021-06-11 08:54:31 +0200

    Add widget

---
+++ messagelist/src/core/widgets/filternamewidget.cpp
@@ -0,0 +1,19 @@
+/*
+  SPDX-FileCopyrightText: 2021 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "filternamewidget.h"
+#include <KLocalizedString>
+#include <QVBoxLayout>
+
+using namespace MessageList::Core;
+FilterNameWidget::FilterNameWidget(QWidget *parent)
+    : QWidget(parent)
+{
+}
+
+FilterNameWidget::~FilterNameWidget()
+{
+}
