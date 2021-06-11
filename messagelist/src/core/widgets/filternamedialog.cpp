9a33cf0bbe23095f18531a57d970d91ae79a80af
Laurent Montel <montel@kde.org>
2021-06-11 08:20:28 +0200

    Add filter name dialog

---
+++ messagelist/src/core/widgets/filternamedialog.cpp
@@ -0,0 +1,20 @@
+/*
+  SPDX-FileCopyrightText: 2021 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "filternamedialog.h"
+#include <KLocalizedString>
+#include <QDialogButtonBox>
+#include <QVBoxLayout>
+
+using namespace MessageList::Core;
+FilterNameDialog::FilterNameDialog(QWidget *parent)
+    : QDialog(parent)
+{
+}
+
+FilterNameDialog::~FilterNameDialog()
+{
+}
---
+++ messagelist/src/core/widgets/filternamedialog.h
@@ -0,0 +1,24 @@
+/*
+  SPDX-FileCopyrightText: 2021 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "messagelist_private_export.h"
+#include <QDialog>
+
+namespace MessageList
+{
+namespace Core
+{
+class MESSAGELIST_TESTS_EXPORT FilterNameDialog : public QDialog
+{
+    Q_OBJECT
+public:
+    explicit FilterNameDialog(QWidget *parent = nullptr);
+    ~FilterNameDialog() override;
+};
+}
+}
