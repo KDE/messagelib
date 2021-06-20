f3639838b1876c62bed4e386d8756d24d13af63d
Laurent Montel <montel@kde.org>
2021-06-20 19:34:24 +0200

    Fix debug info

--- messagelist/src/core/manager.cpp
+++ messagelist/src/core/manager.cpp
@@ -77,7 +77,7 @@ void Manager::registerWidget(Widget *pWidget)
 void Manager::unregisterWidget(Widget *pWidget)
 {
     if (!mInstance) {
-        qCWarning(MESSAGELIST_LOG) << ("ERROR: MessageList::Manager::unregisterWidget() called when Manager::mInstance is 0");
+        qCWarning(MESSAGELIST_LOG) << ("ERROR: MessageList::Manager::unregisterWidget() called when Manager::mInstance is null");
         return;
     }
 
