4b05949cb90f1c9cab49e35addac25c4f158a29f
Laurent Montel <montel@kde.org>
2022-01-01 16:17:07 +0100

    GIT_SILENT: happy new year

--- messageviewer/src/widgets/vcardviewer.cpp
+++ messageviewer/src/widgets/vcardviewer.cpp
@@ -1,6 +1,6 @@
 /* This file is part of the KDE project
    SPDX-FileCopyrightText: 2002 Daniel Molkentin <molkentin@kde.org>
-   SPDX-FileCopyrightText: 2013-2021 Laurent Montel <montel@kde.org>
+   SPDX-FileCopyrightText: 2013-2022 Laurent Montel <montel@kde.org>
 
    SPDX-License-Identifier: GPL-2.0-or-later
  */
--- webengineviewer/src/findbar/findbarwebengineview.h
+++ webengineviewer/src/findbar/findbarwebengineview.h
@@ -1,4 +1,4 @@
-/* SPDX-FileCopyrightText: 2016-2021 Laurent Montel <montel@kde.org>
+/* SPDX-FileCopyrightText: 2016-2022 Laurent Montel <montel@kde.org>
  *
  * SPDX-License-Identifier: LGPL-2.0-or-later
  */
