102454d6f9f518c35f95688282eafc58eeec8da5
Laurent Montel <montel@kde.org>
2022-01-26 07:35:59 +0100

    Add autotest

---
+++ messageviewer/src/openurlwith/autotests/openurlwithmanagertest.cpp
@@ -0,0 +1,23 @@
+/*
+   SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "openurlwithmanagertest.h"
+#include "openurlwith/openurlwithmanager.h"
+#include <QStandardPaths>
+#include <QTest>
+QTEST_MAIN(OpenUrlWithManagerTest)
+
+OpenUrlWithManagerTest::OpenUrlWithManagerTest(QObject *parent)
+    : QObject{parent}
+{
+    QStandardPaths::setTestModeEnabled(true);
+}
+
+void OpenUrlWithManagerTest::shouldHaveDefaultValues()
+{
+    MessageViewer::OpenUrlWithManager w;
+    QVERIFY(w.openWithUrlInfo().isEmpty());
+}
