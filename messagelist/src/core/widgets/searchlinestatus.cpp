2826918b2f5e0932469729e8bd9480b7d34b0982
Laurent Montel <montel@kde.org>
2022-03-29 07:09:21 +0200

    Adapt include for 5.93

--- messagelist/src/core/widgets/searchlinestatus.cpp
+++ messagelist/src/core/widgets/searchlinestatus.cpp
@@ -10,6 +10,10 @@
 #include "filternamedialog.h"
 #include "filtersavedmenu.h"
 #include "messagelist_debug.h"
+#include <kconfigwidgets_version.h>
+#if KCONFIGWIDGETS_VERSION >= QT_VERSION_CHECK(5, 93, 0)
+#include <KStatefulBrush> // was moved to own header in 5.93.0
+#endif
 
 #include <KColorScheme>
 #include <KIconEngine>
