ad11142e9a4c6bc3c53ffcb9126b5e7f647837d5
Laurent Montel <montel@kde.org>
2022-05-09 07:02:19 +0200

    improve autotest

--- messageviewer/src/mdn/autotests/mdnwarningwidgettest.cpp
+++ messageviewer/src/mdn/autotests/mdnwarningwidgettest.cpp
@@ -6,6 +6,7 @@
 #include "mdnwarningwidgettest.h"
 #include "mdn/mdnwarningwidget.h"
 
+#include <QAction>
 #include <QTest>
 
 QTEST_MAIN(MDNWarningWidgetTest)
@@ -22,7 +23,16 @@ void MDNWarningWidgetTest::shouldHaveDefaultValues()
     QVERIFY(w.wordWrap());
 
     QCOMPARE(w.messageType(), KMessageWidget::Information);
-    // mIgnoreAction->setObjectName(QStringLiteral("mIgnoreAction"));
-    //  mSendAction->setObjectName(QStringLiteral("mSendAction"));
-    //  TODO
+    QCOMPARE(w.actions().count(), 2);
+    const auto acts{w.actions()};
+    for (const auto &name : {QStringLiteral("mIgnoreAction"), QStringLiteral("mSendAction")}) {
+        bool found = false;
+        for (auto a : acts) {
+            if (a->objectName() == name) {
+                found = true;
+                break;
+            }
+        }
+        QVERIFY(found);
+    }
 }
