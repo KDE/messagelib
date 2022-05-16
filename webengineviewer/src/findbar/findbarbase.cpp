13f1365467593472f0626313dabbe2dd8cb28a01
Laurent Montel <montel@kde.org>
2022-05-16 07:57:30 +0200

    We depend against kf5.94 now

--- webengineviewer/src/findbar/findbarbase.cpp
+++ webengineviewer/src/findbar/findbarbase.cpp
@@ -5,10 +5,7 @@
  */
 
 #include "findbarbase.h"
-#include <kconfigwidgets_version.h>
-#if KCONFIGWIDGETS_VERSION >= QT_VERSION_CHECK(5, 93, 0)
-#include <KStatefulBrush> // was moved to own header in 5.93.0
-#endif
+#include <KStatefulBrush>
 
 #include <PimCommon/LineEditWithCompleterNg>
 
