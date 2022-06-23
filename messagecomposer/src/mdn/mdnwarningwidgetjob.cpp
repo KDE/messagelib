950e2a2e0c02a9e38d4c10b2e83de10aa32899f2
Laurent Montel <montel@kde.org>
2022-06-23 21:47:21 +0200

    Add mdnwarningwidgetjob

---
+++ messagecomposer/src/mdn/mdnwarningwidgetjob.cpp
@@ -0,0 +1,82 @@
+/*
+  SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "mdnwarningwidgetjob.h"
+#include <MessageComposer/MDNAdviceHelper>
+#include <MessageComposer/MessageFactoryNG>
+#include <MessageComposer/Util>
+#include <MessageViewer/MessageViewerSettings>
+using namespace MessageViewer;
+
+MDNWarningWidgetJob::MDNWarningWidgetJob(QObject *parent)
+    : QObject{parent}
+{
+}
+
+MDNWarningWidgetJob::~MDNWarningWidgetJob() = default;
+
+bool MDNWarningWidgetJob::canStart() const
+{
+    return mItem.isValid();
+}
+
+void MDNWarningWidgetJob::start()
+{
+    if (canStart()) {
+        deleteLater();
+        return;
+    }
+    KMime::Message::Ptr msg = MessageComposer::Util::message(mItem);
+    int mode = MessageViewer::MessageViewerSettings::self()->defaultPolicy();
+    KMime::MDN::SendingMode s = KMime::MDN::SentAutomatically; // set to manual if asked user
+    QPair<QString, bool> mdnInfo;
+    if (!mode || (mode < 0) || (mode > 3)) {
+        // Nothing
+    } else {
+        if (MessageComposer::MessageFactoryNG::MDNMDNUnknownOption(msg)) {
+            mdnInfo = MessageComposer::MDNAdviceHelper::instance()->mdnMessageText("mdnUnknownOption");
+            s = KMime::MDN::SentManually;
+            // TODO set type to Failed as well
+            //      and clear modifiers
+        }
+
+        if (MessageComposer::MessageFactoryNG::MDNConfirmMultipleRecipients(msg)) {
+            mdnInfo = MessageComposer::MDNAdviceHelper::instance()->mdnMessageText("mdnMultipleAddressesInReceiptTo");
+            s = KMime::MDN::SentManually;
+        }
+
+        if (MessageComposer::MessageFactoryNG::MDNReturnPathEmpty(msg)) {
+            mdnInfo = MessageComposer::MDNAdviceHelper::instance()->mdnMessageText("mdnReturnPathEmpty");
+            s = KMime::MDN::SentManually;
+        }
+
+        if (MessageComposer::MessageFactoryNG::MDNReturnPathNotInRecieptTo(msg)) {
+            mdnInfo = MessageComposer::MDNAdviceHelper::instance()->mdnMessageText("mdnReturnPathNotInReceiptTo");
+            s = KMime::MDN::SentManually;
+        }
+
+        if (MessageComposer::MessageFactoryNG::MDNRequested(msg)) {
+            if (s != KMime::MDN::SentManually) {
+                // don't ask again if user has already been asked. use the users' decision
+                mdnInfo = MessageComposer::MDNAdviceHelper::instance()->mdnMessageText("mdnNormalAsk");
+            }
+        } else { // if message doesn't have a disposition header, never send anything.
+            mode = 0;
+        }
+    }
+    Q_EMIT showMdnInfo(mdnInfo);
+    deleteLater();
+}
+
+const Akonadi::Item &MDNWarningWidgetJob::item() const
+{
+    return mItem;
+}
+
+void MDNWarningWidgetJob::setItem(const Akonadi::Item &newItem)
+{
+    mItem = newItem;
+}
---
+++ messagecomposer/src/mdn/mdnwarningwidgetjob.h
@@ -0,0 +1,38 @@
+/*
+  SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "messageviewer_export.h"
+#include <Akonadi/Item>
+#include <QObject>
+namespace MessageViewer
+{
+/**
+ * @brief The MDNWarningWidget class
+ * @author Laurent Montel <montel@kde.org>
+ */
+class MESSAGEVIEWER_EXPORT MDNWarningWidgetJob : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MDNWarningWidgetJob(QObject *parent = nullptr);
+    ~MDNWarningWidgetJob() override;
+
+    void start();
+
+    Q_REQUIRED_RESULT const Akonadi::Item &item() const;
+    void setItem(const Akonadi::Item &newItem);
+
+    Q_REQUIRED_RESULT bool canStart() const;
+
+Q_SIGNALS:
+    void showMdnInfo(const QPair<QString, bool> &mdnInfo);
+
+private:
+    Akonadi::Item mItem;
+};
+}
