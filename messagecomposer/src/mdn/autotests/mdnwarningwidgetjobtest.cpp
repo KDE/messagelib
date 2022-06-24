2e2da364cd0fe5ddcf0dbdfa906f944a1c671a7a
Laurent Montel <montel@kde.org>
2022-06-24 07:25:01 +0200

    Add autotest

---
+++ messagecomposer/src/mdn/autotests/mdnwarningwidgetjobtest.cpp
@@ -0,0 +1,21 @@
+/*
+  SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-only
+*/
+
+#include "mdnwarningwidgetjobtest.h"
+#include "messagecomposer/mdnwarningwidgetjob.h"
+#include <QTest>
+QTEST_GUILESS_MAIN(MDNWarningWidgetJobTest)
+MDNWarningWidgetJobTest::MDNWarningWidgetJobTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void MDNWarningWidgetJobTest::shouldHaveDefaultValues()
+{
+    MessageComposer::MDNWarningWidgetJob job;
+    QVERIFY(!job.item().isValid());
+    QVERIFY(!job.canStart());
+}
---
+++ messagecomposer/src/mdn/autotests/mdnwarningwidgetjobtest.h
@@ -0,0 +1,19 @@
+/*
+  SPDX-FileCopyrightText: 2022 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-only
+*/
+
+#pragma once
+
+#include <QObject>
+
+class MDNWarningWidgetJobTest : public QObject
+{
+    Q_OBJECT
+public:
+    explicit MDNWarningWidgetJobTest(QObject *parent = nullptr);
+    ~MDNWarningWidgetJobTest() override = default;
+private Q_SLOTS:
+    void shouldHaveDefaultValues();
+};
