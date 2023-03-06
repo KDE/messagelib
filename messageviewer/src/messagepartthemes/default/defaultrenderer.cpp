31f382059a43b2cc5ec543c7b17e3fb72fa8342c
Ingo Klöcker <dev@ingo-kloecker.de>
2023-03-06 15:46:32 +0100

    Set compliance property with other message part meta data
    Add all meta data of the message part to the rendering context in one
    spot. In particular, set the isCompliant property only once.
    
    GnuPG-bug-id: 6343

--- messageviewer/src/messagepartthemes/default/defaultrenderer.cpp
+++ messageviewer/src/messagepartthemes/default/defaultrenderer.cpp
@@ -507,6 +507,7 @@ void DefaultRendererPrivate::renderSigned(const SignedMessagePart::Ptr &mp, Html
     }
     block.setProperty("isGoodSignature", metaData.isGoodSignature);
     block.setProperty("isCompliant", metaData.isCompliant);
+    block.setProperty("compliance", metaData.compliance);
     block.setProperty("isSMIME", isSMIME);
 
     if (metaData.keyTrust == GpgME::Signature::Unknown) {
@@ -669,8 +670,6 @@ void DefaultRendererPrivate::renderSigned(const SignedMessagePart::Ptr &mp, Html
     block.setProperty("statusStr", statusStr);
     block.setProperty("signClass", mClass);
     block.setProperty("greenCaseWarning", greenCaseWarning);
-    block.setProperty("isCompliant", metaData.isCompliant);
-    block.setProperty("compliance", metaData.compliance);
 #if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
     Grantlee::OutputStream s(htmlWriter->stream());
 #else
