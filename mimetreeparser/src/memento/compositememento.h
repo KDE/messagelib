624cee41de9780e73951913282eceb376fd34788
Daniel Vrátil <dvratil@kde.org>
2023-05-23 21:22:52 +0200

    Fix crash when switching signed/encrypted emails very fast
    Kleo::KeyCache internally uses a QEventLoop to block until it
    finishes initializing. The initialization happens the first
    time the user opens a signed or encrypted email. If they switch
    to another email before the KeyCache is done initializing it
    leads to a crash in MessageViewer, since message pointers change
    while MessageViewer is still "blocked" in OTP.
    
    To avoid the crash, and running a nested event loop in general,
    this change introduces KeyCacheMemento, which allows to wait for
    the KeyCache to initialize asynchronously before the MessagePart
    queries it.
    
    BUGS: 463083

---
+++ mimetreeparser/src/memento/compositememento.h
@@ -0,0 +1,60 @@
+/*
+  SPDX-FileCopyrightText: 2023 Daniel Vrátil <dvratil@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+
+#include "cryptobodypartmemento.h"
+
+#include <memory>
+#include <type_traits>
+
+namespace MimeTreeParser
+{
+
+class CompositeMemento : public CryptoBodyPartMemento
+{
+    Q_OBJECT
+public:
+    explicit CompositeMemento() = default;
+    ~CompositeMemento() override;
+
+    bool start() override;
+    void exec() override;
+
+    void addMemento(CryptoBodyPartMemento *memento);
+
+    QVector<CryptoBodyPartMemento *> mementos() const
+    {
+        return mMementos;
+    }
+
+    auto size() const
+    {
+        return mMementos.size();
+    }
+
+    template<typename T>
+    T *memento() const
+    {
+        auto it = std::find_if(mMementos.begin(), mMementos.end(), [](auto *memento) {
+            return qobject_cast<std::decay_t<T> *>(memento) != nullptr;
+        });
+        if (it != mMementos.cend()) {
+            return static_cast<T *>(*it);
+        }
+
+        return nullptr;
+    }
+
+private Q_SLOTS:
+    void subMementoFinished();
+
+private:
+    QVector<CryptoBodyPartMemento *> mMementos;
+    int mRunningMementos = 0;
+};
+
+} // namespace
---
+++ mimetreeparser/src/memento/keycachememento.cpp
@@ -0,0 +1,50 @@
+/*
+  SPDX-FileCopyrightText: 2023 Daniel Vrátil <dvratil@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "keycachememento.h"
+
+#include <QTimer>
+
+using namespace MimeTreeParser;
+
+KeyCacheMemento::KeyCacheMemento(const std::shared_ptr<Kleo::KeyCache> &keyCache, GpgME::Protocol protocol)
+    : m_keyCache(keyCache)
+    , m_protocol(protocol)
+{
+}
+
+KeyCacheMemento::~KeyCacheMemento() = default;
+
+bool KeyCacheMemento::start()
+{
+    if (m_keyCache->initialized()) {
+        // delay completion
+        QTimer::singleShot(0, this, &KeyCacheMemento::slotKeyCacheInitialized);
+    } else {
+        connect(m_keyCache.get(), &Kleo::KeyCache::keyListingDone, this, &KeyCacheMemento::slotKeyCacheInitialized);
+        m_keyCache->startKeyListing(m_protocol);
+    }
+
+    setRunning(true);
+    return true;
+}
+
+void KeyCacheMemento::exec()
+{
+    setRunning(true);
+    if (!m_keyCache->initialized()) {
+        // This will start a nested eventloop internally that will block until the
+        // key cache is initialized and can return a result.
+        m_keyCache->findByFingerprint("");
+    }
+    setRunning(false);
+}
+
+void KeyCacheMemento::slotKeyCacheInitialized()
+{
+    setRunning(false);
+    notify();
+}
