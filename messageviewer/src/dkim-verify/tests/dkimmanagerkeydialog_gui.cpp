7130e214df779ba5c1ee85400170b957307563b3
Laurent Montel <montel@kde.org>
2023-06-08 13:32:51 +0200

    Add test apps

---
+++ messageviewer/src/dkim-verify/tests/dkimmanagerkeydialog_gui.cpp
@@ -0,0 +1,18 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "dkim-verify/dkimmanagerkeydialog.h"
+#include <QApplication>
+#include <QCommandLineParser>
+int main(int argc, char **argv)
+{
+    QApplication app(argc, argv);
+
+    MessageViewer::DKIMManagerKeyDialog dlg;
+    dlg.exec();
+    app.exec();
+    return 0;
+}
