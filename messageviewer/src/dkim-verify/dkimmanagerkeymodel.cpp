fca7ac85bc1237cc9cfe6bcd6b0d28b8c4d97f6d
Laurent Montel <montel@kde.org>
2023-06-08 13:33:16 +0200

    improve model

--- messageviewer/src/dkim-verify/dkimmanagerkeymodel.cpp
+++ messageviewer/src/dkim-verify/dkimmanagerkeymodel.cpp
@@ -5,12 +5,96 @@
 */
 
 #include "dkimmanagerkeymodel.h"
+using namespace MessageViewer;
 
 DKIMManagerKeyModel::DKIMManagerKeyModel(QObject *parent)
     : QAbstractListModel{parent}
 {
 }
 
-DKIMManagerKeyModel::~DKIMManagerKeyModel()
+DKIMManagerKeyModel::~DKIMManagerKeyModel() = default;
+
+QList<MessageViewer::KeyInfo> DKIMManagerKeyModel::keyInfos() const
 {
+    return mKeyInfos;
+}
+
+void DKIMManagerKeyModel::setKeyInfos(const QList<MessageViewer::KeyInfo> &newKeyInfos)
+{
+    beginResetModel();
+    mKeyInfos = newKeyInfos;
+    endResetModel();
+}
+
+int DKIMManagerKeyModel::rowCount(const QModelIndex &parent) const
+{
+    if (parent.isValid()) {
+        return 0; // flat model
+    }
+    return mKeyInfos.count();
+}
+
+int DKIMManagerKeyModel::columnCount(const QModelIndex &parent) const
+{
+    Q_UNUSED(parent)
+    return static_cast<int>(DKIMManagerKeyRoles::LastColumn) + 1;
+}
+
+QVariant DKIMManagerKeyModel::data(const QModelIndex &index, int role) const
+{
+    if (index.row() < 0 || index.row() >= mKeyInfos.count()) {
+        return {};
+    }
+    const KeyInfo &keyInfo = mKeyInfos.at(index.row());
+    if (role != Qt::DisplayRole) {
+        return {};
+    }
+    switch (static_cast<DKIMManagerKeyRoles>(index.column())) {
+    case KeyRole: {
+        return keyInfo.keyValue;
+    }
+    case SelectorRole:
+        return keyInfo.selector;
+    case DomainRole:
+        return keyInfo.domain;
+    case StoredAtDateTimeRole:
+        return keyInfo.storedAtDateTime;
+    case LastUsedDateTimeRole:
+        return keyInfo.lastUsedDateTime;
+    }
+    return {};
+}
+
+void DKIMManagerKeyModel::clear()
+{
+    if (!mKeyInfos.isEmpty()) {
+        beginResetModel();
+        mKeyInfos.clear();
+        endResetModel();
+    }
+}
+
+bool DKIMManagerKeyModel::insertKeyInfo(const KeyInfo &keyInfo)
+{
+    bool found = false;
+    if (/*keyInfo.isValid()*/ 1) {
+        auto it = std::find_if(mKeyInfos.cbegin(), mKeyInfos.cend(), [keyInfo](const KeyInfo &key) {
+            return key == keyInfo;
+        });
+        if (it == mKeyInfos.cend()) {
+            beginInsertRows(QModelIndex(), mKeyInfos.count() - 1, mKeyInfos.count());
+            mKeyInfos.append(keyInfo);
+            endInsertRows();
+        } else {
+            found = true;
+        }
+    }
+    return found;
+}
+
+void DKIMManagerKeyModel::removeKeyInfo(const KeyInfo &keyInfo)
+{
+    beginResetModel();
+    // TODO
+    endResetModel();
 }
--- messageviewer/src/dkim-verify/dkimmanagerkeymodel.h
+++ messageviewer/src/dkim-verify/dkimmanagerkeymodel.h
@@ -4,13 +4,40 @@
    SPDX-License-Identifier: LGPL-2.0-or-later
 */
 #pragma once
+#include "dkimmanagerkey.h"
 #include "messageviewer_export.h"
 #include <QAbstractListModel>
-
+#include <QList>
+namespace MessageViewer
+{
 class MESSAGEVIEWER_EXPORT DKIMManagerKeyModel : public QAbstractListModel
 {
     Q_OBJECT
 public:
+    enum DKIMManagerKeyRoles {
+        KeyRole,
+        SelectorRole,
+        DomainRole,
+        StoredAtDateTimeRole,
+        LastUsedDateTimeRole,
+        LastColumn = LastUsedDateTimeRole,
+    };
+
     explicit DKIMManagerKeyModel(QObject *parent = nullptr);
     ~DKIMManagerKeyModel() override;
+
+    Q_REQUIRED_RESULT QList<MessageViewer::KeyInfo> keyInfos() const;
+    void setKeyInfos(const QList<MessageViewer::KeyInfo> &newKeyInfos);
+
+    Q_REQUIRED_RESULT int rowCount(const QModelIndex &parent = QModelIndex()) const override;
+    Q_REQUIRED_RESULT int columnCount(const QModelIndex &parent = QModelIndex()) const override;
+    Q_REQUIRED_RESULT QVariant data(const QModelIndex &index, int role = Qt::DisplayRole) const override;
+    Q_REQUIRED_RESULT bool insertKeyInfo(const MessageViewer::KeyInfo &keyInfo);
+    void removeKeyInfo(const MessageViewer::KeyInfo &keyInfo);
+
+    void clear();
+
+private:
+    QList<MessageViewer::KeyInfo> mKeyInfos;
 };
+}
