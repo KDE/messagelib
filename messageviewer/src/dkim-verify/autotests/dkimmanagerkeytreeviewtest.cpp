8a7e811ce92431f481175ea356fa09231629759c
Laurent Montel <montel@kde.org>
2023-06-08 19:23:33 +0200

    Add autotest

---
+++ messageviewer/src/dkim-verify/autotests/dkimmanagerkeytreeviewtest.cpp
@@ -0,0 +1,24 @@
+/*
+   SPDX-FileCopyrightText: 2023 Laurent Montel <montel@kde.org>
+
+   SPDX-License-Identifier: LGPL-2.0-or-later
+*/
+
+#include "dkimmanagerkeytreeviewtest.h"
+#include "dkim-verify/dkimmanagerkeytreeview.h"
+#include <QTest>
+QTEST_MAIN(DKIMManagerKeyTreeViewTest)
+DKIMManagerKeyTreeViewTest::DKIMManagerKeyTreeViewTest(QObject *parent)
+    : QObject{parent}
+{
+}
+
+void DKIMManagerKeyTreeViewTest::shouldHaveDefaultValues()
+{
+    MessageViewer::DKIMManagerKeyTreeView w;
+    QVERIFY(w.uniformRowHeights());
+    QVERIFY(w.alternatingRowColors());
+    QVERIFY(!w.rootIsDecorated());
+    QCOMPARE(w.selectionBehavior(), QAbstractItemView::SelectRows);
+    QCOMPARE(w.selectionMode(), QAbstractItemView::ExtendedSelection);
+}
