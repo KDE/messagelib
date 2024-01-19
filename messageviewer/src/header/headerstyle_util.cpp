KDE/messagelib

commit 2cf129accb92454e66dde37017086a211125ff84

Port away from deprecated time_t based KMime API
We probably also want to remove the remaining time_t uses in here, that
will overflow in less than 15 years.

messageviewer/src/header/headerstyle_util.cpp
@@ -55,16 +55,15 @@ QString HeaderStyleUtil::dateString(const QDateTime &dateTime, HeaderStyleUtilDa
         return i18nc("Unknown date", "Unknown");
     }
 
-    const time_t unixTime = dateTime.toSecsSinceEpoch();
     switch (dateFormat) {
     case ShortDate:
-        return KMime::DateFormatter::formatDate(KMime::DateFormatter::Localized, unixTime);
+        return KMime::DateFormatter::formatDate(KMime::DateFormatter::Localized, dateTime);
     case LongDate:
-        return KMime::DateFormatter::formatDate(KMime::DateFormatter::CTime, unixTime);
+        return KMime::DateFormatter::formatDate(KMime::DateFormatter::CTime, dateTime);
     case FancyShortDate:
-        return KMime::DateFormatter::formatDate(KMime::DateFormatter::Fancy, unixTime);
+        return KMime::DateFormatter::formatDate(KMime::DateFormatter::Fancy, dateTime);
     case FancyLongDate:
-        return KMime::DateFormatter::formatDate(KMime::DateFormatter::Fancy, unixTime, QString(), false);
+        return KMime::DateFormatter::formatDate(KMime::DateFormatter::Fancy, dateTime, QString(), false);
     case CustomDate:
     default:
         return dateStr(dateTime);
@@ -202,9 +201,8 @@ QString HeaderStyleUtil::imgToDataUrl(const QImage &image) const
 
 QString HeaderStyleUtil::dateStr(const QDateTime &dateTime)
 {
-    const time_t unixTime = dateTime.toSecsSinceEpoch();
     return KMime::DateFormatter::formatDate(static_cast<KMime::DateFormatter::FormatType>(MessageCore::MessageCoreSettings::self()->dateFormat()),
-                                            unixTime,
+                                            dateTime,
                                             MessageCore::MessageCoreSettings::self()->customDateFormat());
 }
 

messagecomposer/autotests/messagefactoryngtest.cpp
@@ -830,7 +830,7 @@ void MessageFactoryTest::testCreateMDN()
     QString mdnContent = QString::fromLatin1(
         "The message sent on %1 to %2 with subject \"%3\" has been displayed. "
         "This is no guarantee that the message has been read or understood.");
-    mdnContent = mdnContent.arg(KMime::DateFormatter::formatDate(KMime::DateFormatter::Localized, msg->date()->dateTime().toSecsSinceEpoch()))
+    mdnContent = mdnContent.arg(KMime::DateFormatter::formatDate(KMime::DateFormatter::Localized, msg->date()->dateTime()))
                      .arg(msg->to()->asUnicodeString(), msg->subject()->asUnicodeString());
 
     QCOMPARE_OR_DIFF(Util::findTypeInMessage(mdn.data(), "multipart", "report")->contents().at(0)->body(), mdnContent.toLatin1());

messagecomposer/src/helper/messagefactoryng.cpp
@@ -906,7 +906,7 @@ QString MessageFactoryNG::replaceHeadersInString(const KMime::Message::Ptr &msg,
     QString result = s;
     static QRegularExpression rx{QStringLiteral("\\$\\{([a-z0-9-]+)\\}"), QRegularExpression::CaseInsensitiveOption};
 
-    const QString sDate = KMime::DateFormatter::formatDate(KMime::DateFormatter::Localized, msg->date()->dateTime().toSecsSinceEpoch());
+    const QString sDate = KMime::DateFormatter::formatDate(KMime::DateFormatter::Localized, msg->date()->dateTime());
     qCDebug(MESSAGECOMPOSER_LOG) << "creating mdn date:" << msg->date()->dateTime().toSecsSinceEpoch() << sDate;
 
     result.replace(QStringLiteral("${date}"), sDate);

messagelist/src/core/item.cpp
@@ -307,7 +307,7 @@ QString Item::formattedDate() const
     if (static_cast<uint>(date()) == static_cast<uint>(-1)) {
         return Manager::instance()->cachedLocalizedUnknownText();
     } else {
-        return Manager::instance()->dateFormatter()->dateString(date());
+        return Manager::instance()->dateFormatter()->dateString(QDateTime::fromSecsSinceEpoch(date()));
     }
 }
 
@@ -316,7 +316,7 @@ QString Item::formattedMaxDate() const
     if (static_cast<uint>(maxDate()) == static_cast<uint>(-1)) {
         return Manager::instance()->cachedLocalizedUnknownText();
     } else {
-        return Manager::instance()->dateFormatter()->dateString(maxDate());
+        return Manager::instance()->dateFormatter()->dateString(QDateTime::fromSecsSinceEpoch(maxDate()));
     }
 }
 

commit 98a89c2a206148ba7270f34b1b2cd4a83d79d7fc

Fix Grantlee header test date/time formatting
This regressed in 2cf129accb as the now consistent use of QDateTime no
longer loses the timezone information. So convert explicitly to local
time where needed.

messageviewer/src/header/headerstyle_util.cpp
@@ -57,13 +57,13 @@ QString HeaderStyleUtil::dateString(const QDateTime &dateTime, HeaderStyleUtilDa
 
     switch (dateFormat) {
     case ShortDate:
-        return KMime::DateFormatter::formatDate(KMime::DateFormatter::Localized, dateTime);
+        return KMime::DateFormatter::formatDate(KMime::DateFormatter::Localized, dateTime.toLocalTime());
     case LongDate:
-        return KMime::DateFormatter::formatDate(KMime::DateFormatter::CTime, dateTime);
+        return KMime::DateFormatter::formatDate(KMime::DateFormatter::CTime, dateTime.toLocalTime());
     case FancyShortDate:
-        return KMime::DateFormatter::formatDate(KMime::DateFormatter::Fancy, dateTime);
+        return KMime::DateFormatter::formatDate(KMime::DateFormatter::Fancy, dateTime.toLocalTime());
     case FancyLongDate:
-        return KMime::DateFormatter::formatDate(KMime::DateFormatter::Fancy, dateTime, QString(), false);
+        return KMime::DateFormatter::formatDate(KMime::DateFormatter::Fancy, dateTime.toLocalTime(), QString(), false);
     case CustomDate:
     default:
         return dateStr(dateTime);
@@ -202,7 +202,7 @@ QString HeaderStyleUtil::imgToDataUrl(const QImage &image) const
 QString HeaderStyleUtil::dateStr(const QDateTime &dateTime)
 {
     return KMime::DateFormatter::formatDate(static_cast<KMime::DateFormatter::FormatType>(MessageCore::MessageCoreSettings::self()->dateFormat()),
-                                            dateTime,
+                                            dateTime.toLocalTime(),
                                             MessageCore::MessageCoreSettings::self()->customDateFormat());
 }
 