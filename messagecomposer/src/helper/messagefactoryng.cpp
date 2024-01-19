KDE/messagelib

commit 18893efcb84f3436a156531acae72511ccfb7cb1

Avoid to duplicate email identity
now we test if "foo@bla.com" <foo@bla.com>" with identity
otherwise it duplicated identity email

messagecomposer/src/helper/messagefactoryng.cpp
@@ -222,7 +222,6 @@ void MessageFactoryNG::createReplyAsync()
                 // Doesn't seem to be a mailing list.
                 auto originalFromList = mOrigMsg->from()->mailboxes();
                 auto originalToList = mOrigMsg->to()->mailboxes();
-
                 if (mIdentityManager->thatIsMe(KMime::Types::Mailbox::listToUnicodeString(originalFromList))
                     && !mIdentityManager->thatIsMe(KMime::Types::Mailbox::listToUnicodeString(originalToList))) {
                     // Sender seems to be one of our own identities and recipient is not,
@@ -232,7 +231,10 @@ void MessageFactoryNG::createReplyAsync()
                 } else {
                     // "Normal" case:  reply to sender.
                     toList = stripMyAddressesFromAddressList(mOrigMsg->to()->mailboxes(), mIdentityManager);
-                    toList += authorMailboxes(mOrigMsg, mMailingListAddresses);
+                    const auto authors = authorMailboxes(mOrigMsg, mMailingListAddresses);
+                    if (!mIdentityManager->thatIsMe(KMime::Types::Mailbox::listToUnicodeString(authors))) {
+                        toList += authors;
+                    }
                 }
             }
 

commit 82679a0c2c8e47fe80795b79ace27370e10ec5a5

Fix replystrategytest
This regressed in 18893efcb8 which didn't handle the case of an email
entirely between our own identities.

messagecomposer/src/helper/messagefactoryng.cpp
@@ -231,7 +231,7 @@ void MessageFactoryNG::createReplyAsync()
                     // "Normal" case:  reply to sender.
                     toList = stripMyAddressesFromAddressList(mOrigMsg->to()->mailboxes(), mIdentityManager);
                     const auto authors = authorMailboxes(mOrigMsg, mMailingListAddresses);
-                    if (!mIdentityManager->thatIsMe(KMime::Types::Mailbox::listToUnicodeString(authors))) {
+                    if (toList.isEmpty() || !mIdentityManager->thatIsMe(KMime::Types::Mailbox::listToUnicodeString(authors))) {
                         toList += authors;
                     }
                 }