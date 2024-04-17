983b9957f590b59577f84fe838eb53614739ee4e
Carl Schwan <carl@carlschwan.eu>
2024-04-17 09:30:03 +0200

    Avoid heap allocation when calculating size with encoding
    We can just declare the content on the stack

--- messagecore/src/attachment/attachmentpart.cpp
+++ messagecore/src/attachment/attachmentpart.cpp
@@ -21,14 +21,11 @@ size_t qHash(const MessageCore::AttachmentPart::Ptr &ptr, size_t seed)
 // TODO move to kmime_util?
 static qint64 sizeWithEncoding(const QByteArray &data, KMime::Headers::contentEncoding encoding) // local
 {
-    auto content = new KMime::Content;
-    content->setBody(data);
-    content->contentTransferEncoding()->setEncoding(encoding);
+    KMime::Content content;
+    content.setBody(data);
+    content.contentTransferEncoding()->setEncoding(encoding);
 
-    const int size = content->size();
-    delete content;
-
-    return size;
+    return content.size();
 }
 
 static KMime::Headers::contentEncoding bestEncodingForTypeAndData(const QByteArray &mimeType, const QByteArray &data)
