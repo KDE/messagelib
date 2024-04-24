20d3ec6385116a9e01859681dfcbc26b20acc311
Volker Krause <vkrause@kde.org>
2024-04-24 22:04:47 +0200

    Fix backoffmodemanagertest
    This needs to reset the state it creates for every run, and needs to
    consider state from its other test functions.

--- webengineviewer/src/checkphishingurl/autotests/backoffmodemanagertest.cpp
+++ webengineviewer/src/checkphishingurl/autotests/backoffmodemanagertest.cpp
@@ -6,6 +6,10 @@
 
 #include "backoffmodemanagertest.h"
 #include "../backoffmodemanager.h"
+#include "../checkphishingurlutil.h"
+
+#include <KConfig>
+
 #include <QStandardPaths>
 #include <QTest>
 QTEST_GUILESS_MAIN(BackOffModeManagerTest)
@@ -13,6 +17,9 @@ BackOffModeManagerTest::BackOffModeManagerTest(QObject *parent)
     : QObject(parent)
 {
     QStandardPaths::setTestModeEnabled(true);
+
+    KConfig config(WebEngineViewer::CheckPhishingUrlUtil::configFileName());
+    config.deleteGroup(QStringLiteral("BackOffMode"));
 }
 
 BackOffModeManagerTest::~BackOffModeManagerTest() = default;
@@ -34,10 +41,11 @@ void BackOffModeManagerTest::shouldStartBackOffMode()
 void BackOffModeManagerTest::shouldIncreaseBackOff()
 {
     WebEngineViewer::BackOffModeManager *manager = WebEngineViewer::BackOffModeManager::self();
+    const auto prevCount = manager->numberOfHttpFailed();
     for (int i = 0; i < 5; ++i) {
         manager->startOffMode();
     }
-    QCOMPARE(manager->numberOfHttpFailed(), 5);
+    QCOMPARE(manager->numberOfHttpFailed(), prevCount + 5);
 }
 
 #include "moc_backoffmodemanagertest.cpp"
