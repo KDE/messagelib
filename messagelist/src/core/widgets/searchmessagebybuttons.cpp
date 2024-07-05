c83dd2414b749bfbc1cb1678619389b9d3c37e55
Laurent Montel <montel@kde.org>
2024-07-05 20:29:23 +0200

    Continue to implement searchmessagebybuttons

--- messagelist/src/core/widgets/searchmessagebybuttons.cpp
+++ messagelist/src/core/widgets/searchmessagebybuttons.cpp
@@ -5,6 +5,8 @@
 */
 
 #include "searchmessagebybuttons.h"
+#include <KLocalizedString>
+#include <QButtonGroup>
 #include <QHBoxLayout>
 #include <QLabel>
 #include <QToolButton>
@@ -13,15 +15,55 @@ using namespace MessageList::Core;
 SearchMessageByButtons::SearchMessageByButtons(QWidget *parent)
     : QWidget{parent}
     , mMainLayout(new QHBoxLayout(this))
+    , mButtonGroup(new QButtonGroup(this))
 {
     mMainLayout->setObjectName(QStringLiteral("mainLayout"));
     mMainLayout->setContentsMargins({});
     mMainLayout->setSpacing(0);
+
+    mButtonGroup->setObjectName(QStringLiteral("mButtonGroup"));
     // TODO
+    connect(mButtonGroup, &QButtonGroup::idClicked, this, [this](int index) {
+        // TODO
+    });
 }
 
 SearchMessageByButtons::~SearchMessageByButtons() = default;
 
 void SearchMessageByButtons::createButtons()
 {
+    createFilterButton(i18nc("@action", "Full Message"), SearchEveryWhere);
+    createFilterButton(i18nc("@action", "Body"), SearchAgainstBody);
+    createFilterButton(i18nc("@action", "Subject"), SearchAgainstSubject);
+    createFilterButton(QString(), SearchAgainstTo); // TODO update text
+    createFilterButton(i18nc("@action", "BCC"), SearchAgainstBcc);
 }
+
+void SearchMessageByButtons::createFilterButton(const QString &text, SearchOption option)
+{
+    auto button = new QToolButton(this);
+    button->setText(text);
+    mButtonGroup->addButton(button, option);
+}
+
+void SearchMessageByButtons::changeSearchAgainstFromOrToText()
+{
+#if 0
+    if (mContainsOutboundMessages) {
+        mSearchAgainstFromOrToAction->setText(i18n("To"));
+    } else {
+        mSearchAgainstFromOrToAction->setText(i18n("From"));
+    }
+#endif
+}
+
+#if 0
+
+mSearchAgainstFromOrToAction = new QAction(mFilterMenu);
+changeSearchAgainstFromOrToText();
+grp->addAction(mSearchAgainstFromOrToAction);
+mFilterMenu->addAction(mSearchAgainstFromOrToAction);
+mSearchAgainstFromOrToAction->setCheckable(true);
+
+connect(grp, &QActionGroup::triggered, this, &SearchLineStatus::slotFilterActionClicked);
+#endif
--- messagelist/src/core/widgets/searchmessagebybuttons.h
+++ messagelist/src/core/widgets/searchmessagebybuttons.h
@@ -8,6 +8,7 @@
 #include "messagelist_private_export.h"
 #include <QWidget>
 class QHBoxLayout;
+class QButtonGroup;
 namespace MessageList
 {
 namespace Core
@@ -16,12 +17,24 @@ class MESSAGELIST_TESTS_EXPORT SearchMessageByButtons : public QWidget
 {
     Q_OBJECT
 public:
+    enum SearchOption {
+        SearchEveryWhere = 1,
+        SearchAgainstBody = 2,
+        SearchAgainstSubject = 4,
+        SearchAgainstFrom = 8,
+        SearchAgainstBcc = 16,
+        SearchAgainstTo = 32
+    };
     explicit SearchMessageByButtons(QWidget *parent = nullptr);
     ~SearchMessageByButtons() override;
 
+    void changeSearchAgainstFromOrToText();
+
 private:
+    MESSAGELIST_NO_EXPORT void createFilterButton(const QString &text, SearchOption option);
     MESSAGELIST_NO_EXPORT void createButtons();
     QHBoxLayout *const mMainLayout;
+    QButtonGroup *const mButtonGroup;
 };
 }
 }
