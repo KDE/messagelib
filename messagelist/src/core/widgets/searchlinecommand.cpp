03770d41045370db6747315a472238c0606d7612
Laurent Montel <montel@kde.org>
2024-09-18 13:38:30 +0200

    Prepare to implement search line command

---
+++ messagelist/src/core/widgets/searchlinecommand.cpp
@@ -0,0 +1,12 @@
+/*
+  SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#include "searchlinecommand.h"
+
+using namespace MessageList::Core;
+SearchLineCommand::SearchLineCommand() = default;
+
+SearchLineCommand::~SearchLineCommand() = default;
---
+++ messagelist/src/core/widgets/searchlinecommand.h
@@ -0,0 +1,20 @@
+/*
+  SPDX-FileCopyrightText: 2024 Laurent Montel <montel@kde.org>
+
+  SPDX-License-Identifier: GPL-2.0-or-later
+*/
+
+#pragma once
+#include "messagelist_private_export.h"
+namespace MessageList
+{
+namespace Core
+{
+class MESSAGELIST_TESTS_EXPORT SearchLineCommand
+{
+public:
+    SearchLineCommand();
+    ~SearchLineCommand();
+};
+}
+}
