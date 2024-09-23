3bd059e69b85273c7dd348c7676bdca63fa58f18
Laurent Montel <laurent@kdab.com>
2024-09-23 18:45:19 +0200

    Add double quote

--- messagelist/src/core/widgets/searchlinecommand.cpp
+++ messagelist/src/core/widgets/searchlinecommand.cpp
@@ -108,6 +108,7 @@ void SearchLineCommand::parseSearchLineCommand(const QString &str)
     // qDebug() << " END " << mSearchLineInfo;
     // TODO add date ?
     // TODO add size: ?
+    // TODO add support for double quote
     // TODO add more email state (spam/starred etc .)
     // We need to extend emailquery or creating query by hand
 }
