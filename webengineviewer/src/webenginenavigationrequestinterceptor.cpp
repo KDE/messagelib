ed80e347c97cd29f662d8ffdd8241bdd2e7f74f6
Albert Astals Cid <aacid@kde.org>
2025-12-12 21:46:17 +0100

    Fix left-click not working on links with target="_blank" in HTML e-mails
    BUGS: 493325

--- webengineviewer/src/webenginenavigationrequestinterceptor.cpp
+++ webengineviewer/src/webenginenavigationrequestinterceptor.cpp
@@ -9,7 +9,7 @@
 using namespace WebEngineViewer;
 
 WebEngineNavigationRequestInterceptor::WebEngineNavigationRequestInterceptor(QWebEnginePage *page)
-    : QWebEnginePage(page)
+    : QWebEnginePage(page->profile(), page)
     , mTargetPage(page)
 {
 }
