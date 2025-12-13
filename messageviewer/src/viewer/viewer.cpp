c34c3f82ab1f2c3118cc365be52f779c039e511c
Laurent Montel <laurent@kdab.com>
2025-12-13 18:21:58 +0100

    Use connect with 4 arguments

--- messageviewer/src/viewer/viewer.cpp
+++ messageviewer/src/viewer/viewer.cpp
@@ -75,7 +75,7 @@ void Viewer::initialize()
 {
     connect(d_ptr, &ViewerPrivate::displayPopupMenu, this, &Viewer::displayPopupMenu);
     connect(d_ptr, &ViewerPrivate::popupMenu, this, &Viewer::popupMenu);
-    connect(d_ptr, SIGNAL(urlClicked(Akonadi::Item, QUrl)), SIGNAL(urlClicked(Akonadi::Item, QUrl)));
+    connect(d_ptr, SIGNAL(urlClicked(Akonadi::Item, QUrl)), this, SIGNAL(urlClicked(Akonadi::Item, QUrl)));
     connect(d_ptr, &ViewerPrivate::requestConfigSync, this, &Viewer::requestConfigSync);
     connect(d_ptr, &ViewerPrivate::makeResourceOnline, this, &Viewer::makeResourceOnline);
     connect(d_ptr, &ViewerPrivate::showReader, this, &Viewer::showReader);
