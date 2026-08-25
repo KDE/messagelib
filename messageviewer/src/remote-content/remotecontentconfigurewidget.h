54d42ffd933db9daf837f0428891cb8d2e1f84a5
Laurent Montel <montel@kde.org>
2026-08-25 14:03:52 +0200

    Remove unused forward declaration

--- messageviewer/src/remote-content/remotecontentconfigurewidget.h
+++ messageviewer/src/remote-content/remotecontentconfigurewidget.h
@@ -15,7 +15,6 @@ class QTreeWidget;
 namespace MessageViewer
 {
 class RemoteContentStatusTypeComboBox;
-class RemoteContentInfo;
 /**
  * \class MessageViewer::RemoteContentWidgetItem
  * \inmodule MessageViewer
