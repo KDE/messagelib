34d83b7e6bbe78cbe79d32b5fa1cd29b5679f47d
Laurent Montel <montel@kde.org>
2026-09-18 22:15:01 +0200

    USe setting variable here

--- messageviewer/src/viewer/webengine/mailwebenginepage.cpp
+++ messageviewer/src/viewer/webengine/mailwebenginepage.cpp
@@ -20,32 +20,35 @@ MailWebEnginePage::MailWebEnginePage(QObject *parent)
 void MailWebEnginePage::initialize()
 {
     const bool enableElement = false;
-    settings()->setAttribute(QWebEngineSettings::JavascriptEnabled, enableElement);
-    settings()->setAttribute(QWebEngineSettings::PluginsEnabled, enableElement);
-    settings()->setAttribute(QWebEngineSettings::JavascriptCanOpenWindows, enableElement);
-    settings()->setAttribute(QWebEngineSettings::JavascriptCanAccessClipboard, enableElement);
-    settings()->setAttribute(QWebEngineSettings::LocalStorageEnabled, enableElement);
-    settings()->setAttribute(QWebEngineSettings::XSSAuditingEnabled, enableElement);
+    auto *const settings = this->settings();
+    settings->setAttribute(QWebEngineSettings::JavascriptEnabled, enableElement);
+    settings->setAttribute(QWebEngineSettings::PluginsEnabled, enableElement);
+    settings->setAttribute(QWebEngineSettings::JavascriptCanOpenWindows, enableElement);
+    settings->setAttribute(QWebEngineSettings::JavascriptCanAccessClipboard, enableElement);
+    settings->setAttribute(QWebEngineSettings::LocalStorageEnabled, enableElement);
+    settings->setAttribute(QWebEngineSettings::XSSAuditingEnabled, enableElement);
     // Default off, needed to load any external urls, we filter them manual
-    settings()->setAttribute(QWebEngineSettings::LocalContentCanAccessRemoteUrls, true);
+    settings->setAttribute(QWebEngineSettings::LocalContentCanAccessRemoteUrls, true);
     // Default on, needed to load our own icons and cid urls. FIXME: Ideally should be off for additional security
-    settings()->setAttribute(QWebEngineSettings::LocalContentCanAccessFileUrls, true);
-    settings()->setAttribute(QWebEngineSettings::ErrorPageEnabled, enableElement);
-    settings()->setAttribute(QWebEngineSettings::HyperlinkAuditingEnabled, enableElement);
-    settings()->setAttribute(QWebEngineSettings::FullScreenSupportEnabled, enableElement);
-    settings()->setAttribute(QWebEngineSettings::WebGLEnabled, enableElement);
-    settings()->setAttribute(QWebEngineSettings::AutoLoadIconsForPage, enableElement);
-    settings()->setAttribute(QWebEngineSettings::Accelerated2dCanvasEnabled, enableElement);
-    settings()->setAttribute(QWebEngineSettings::FocusOnNavigationEnabled, enableElement);
-    settings()->setAttribute(QWebEngineSettings::AllowRunningInsecureContent, enableElement);
-    settings()->setAttribute(QWebEngineSettings::PdfViewerEnabled, enableElement);
-    settings()->setAttribute(QWebEngineSettings::NavigateOnDropEnabled, enableElement);
-    profile()->setPersistentCookiesPolicy(QWebEngineProfile::NoPersistentCookies);
-    profile()->setHttpCacheType(QWebEngineProfile::MemoryHttpCache);
+    settings->setAttribute(QWebEngineSettings::LocalContentCanAccessFileUrls, true);
+    settings->setAttribute(QWebEngineSettings::ErrorPageEnabled, enableElement);
+    settings->setAttribute(QWebEngineSettings::HyperlinkAuditingEnabled, enableElement);
+    settings->setAttribute(QWebEngineSettings::FullScreenSupportEnabled, enableElement);
+    settings->setAttribute(QWebEngineSettings::WebGLEnabled, enableElement);
+    settings->setAttribute(QWebEngineSettings::AutoLoadIconsForPage, enableElement);
+    settings->setAttribute(QWebEngineSettings::Accelerated2dCanvasEnabled, enableElement);
+    settings->setAttribute(QWebEngineSettings::FocusOnNavigationEnabled, enableElement);
+    settings->setAttribute(QWebEngineSettings::AllowRunningInsecureContent, enableElement);
+    settings->setAttribute(QWebEngineSettings::PdfViewerEnabled, enableElement);
+    settings->setAttribute(QWebEngineSettings::NavigateOnDropEnabled, enableElement);
+
+    auto *const profile = this->profile();
+    profile->setPersistentCookiesPolicy(QWebEngineProfile::NoPersistentCookies);
+    profile->setHttpCacheType(QWebEngineProfile::MemoryHttpCache);
 
     const QFontInfo font(QFontDatabase::systemFont(QFontDatabase::GeneralFont));
-    settings()->setFontFamily(QWebEngineSettings::StandardFont, font.family());
-    settings()->setFontSize(QWebEngineSettings::DefaultFontSize, font.pixelSize());
+    settings->setFontFamily(QWebEngineSettings::StandardFont, font.family());
+    settings->setFontSize(QWebEngineSettings::DefaultFontSize, font.pixelSize());
     connect(this, &QWebEnginePage::permissionRequested, this, &MailWebEnginePage::slotFeaturePermissionRequested);
 }
 
